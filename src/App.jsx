ab2f9a83e2c15d71a9f608199b2ec46dd5ab0a65
anchal-thakur644 <anchalt2345@gmail.com>
2025-04-03 08:38:33 +0000

    some new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
  
-console.log("hello students");
+console.log("hello anchal");
   return (
     <>
       <div>
