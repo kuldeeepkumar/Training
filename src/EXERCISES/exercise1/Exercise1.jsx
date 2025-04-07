4cc19e5d95300eb2bd82203878b405f02095506b
preet249 <bandhanpreet872@gmail.com>
2025-04-07 06:30:52 +0000

    Exercise1 is done

---
+++ src/EXERCISES/exercise1/Exercise1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Variables from './Variables'
+import Opertors from './Opertors'
+
+const Exercise1 = () => {
+  return (
+  <>
+  <Opertors/>
+  </>
+  ) 
+}
+
+export default Exercise1
