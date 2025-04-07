232b7b5202f570d1e5b11393e7e2a1b9c3585c3a
geetika9872 <geetikageetika046@gmail.com>
2025-04-07 07:13:37 +0000

    Exercise1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Exercise1 from './exercises/exersise1/Exercise1'
 
 function App() {
 console.log("hello")  
 
   return (
     <>
-      <div>
-        <h1>geetika</h1>
-      </div>
+  <Exercise1/>
     </>
   )
 }
---
+++ src/exercises/exersise1/Exercise1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Variables from './Variables'
+import Datatypes from './Datatypes'
+import Operator from './Operators'
+
+const Exercise1 = () => {
+  return (
+  <>
+  <Variables/>
+  <Datatypes/>
+  <Operator/>
+  </>
+  )
+}
+
+export default Exercise1
