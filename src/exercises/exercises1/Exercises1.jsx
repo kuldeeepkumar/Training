3b458be9eddc55852e1daace06011b5849689fe2
shikha-kashab <shikhakashab@gmail.com>
2025-04-07 08:16:10 +0000

    Exercises1 is done

---
+++ src/exercises/exercises1/Exercises1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Variables from './Variables'
+import Operators from  './Operators'
+import Datatypes from './Datatypes'
+
+const Exercises1 = () => {
+  return (
+  <>
+
+  <Variables/>
+  <Operators/>
+  <Datatypes/>
+  
+
+  </>
+  )
+}
+
+export default Exercises1
