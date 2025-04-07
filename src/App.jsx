2f0dd0a9db64db23a3e6f9e0ee709a65870f92e6
geetika9872 <geetikageetika046@gmail.com>
2025-04-07 07:15:07 +0000

    import statement issue is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Exercise1 from './exercises/exersise1/Exercise1'
+import Exercise1 from './exercises/exercise1/Exercise1'
 
 function App() {
 console.log("hello")  
