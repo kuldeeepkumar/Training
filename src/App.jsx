1d81393a85c35c1e25c5f6e1eb40e91f7e2d3052
paramveerkaur75 <bhatiaparamveer69@gmail.com>
2025-04-07 10:00:18 +0000

    exercise 2 is started and work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
 import Exercise1 from './exercises/exercise 1/Exercise1'
+import Exercise2 from './exercises/exercise 1/exercise 2/exercise2';
+
 
 function App() {
 console.log("hello")
 
   return (
     <>
-<Exercise1 />
+{/*<Exercise1 />*/}
+<Exercise2/>
     </>
   )
 }
---
+++ src/exercises/exercise 1/exercise 2/exercise2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConditionalStatements from './ConditionalStatements'
+
+const exercise2 = () => {
+  return (
+    <>
+    <ConditionalStatements/>
+    </>
+  )
+}
+
+export default exercise2
