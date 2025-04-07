a82ccc6cf26454f89d461e19d84b96d6a8a87c4b
shikha-kashab <shikhakashab@gmail.com>
2025-04-07 08:16:40 +0000

    Exercises2 vis done

---
+++ src/exercises/exercises2/Exercises2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ConditionalStatements from './ConditionalStatements'
+
+const Exercises2 = () => {
+  return (
+    <>
+    <ConditionalStatements/>
+    
+    
+    
+    </>
+  )
+}
+
+export default Exercises2
