15da5a172fe53e84ce1d5347af4337f61c7609e5
geetika9872 <geetikageetika046@gmail.com>
2025-04-07 10:00:30 +0000

    exercise 2 is startted and working in progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Exercise1 from './exercises/exercise1/Exercise1'
+import Exercise2 from './exercises/exercise2/Exercise2'
 
 function App() {
 console.log("hello")  
 
   return (
     <>
-  <Exercise1/>
+  {/* <Exercise1/> */}
+  <Exercise2/>
     </>
   )
 }
