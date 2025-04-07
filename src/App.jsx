7f4eb4b7c5a205bcf8d9cea5d874ea2be989fb3b
KULDEEP KUMAR <113312937+kuldeeepkumar@users.noreply.github.com>
2025-04-07 06:52:12 +0000

    Exercise 2 Conditional Statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Exercise1 from './exercises/exercise1/Exercise1'
+import Exercise2 from './exercises/exercise2/Exercise2'
 
 function App() {
 
   return (
  <>
- <Exercise1/>
+ {/* <Exercise1/> */}
+ <Exercise2/>
  </>
   )
 }
