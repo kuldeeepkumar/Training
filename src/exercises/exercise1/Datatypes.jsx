3d32107ce78209ab9f3d6dab994ddede03fde594
sheetal-kaur <pinko7018@gmail.com>
2025-04-08 06:45:29 +0000

    Exercise1 is done

---
+++ src/exercises/exercise1/Datatypes.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Datatypes = () => {
+     // Data types
+   // String
+   let yourName = "Sheetal";
+   let greeting = "Hello";
+   let sentence = `My name is ${yourName}`; // Template literals
+
+   console.log(yourName)
+   console.log(greeting)
+   console.log( sentence)
+ 
+   //  Number
+ 
+   let age = 30;
+   let price = 19.99;
+
+   console.log(age)
+   console.log(price)
+
+ 
+   // boolean
+   let isStudent = true;
+   let isTeacher = false;
+
+   console.log(isStudent)
+   console.log(isTeacher)
+
+ 
+   // Object
+   let person = {
+     name: "Alice",
+     age: 30,
+   };
+
+   console.log(person)
+ 
+   // Array
+   let fruits = ["apple", "banana", "cherry"];
+
+   console.log(fruits)
+ 
+   // undefined
+   let car;
+   console.log(car); // Output: undefined
+ 
+   // null
+   let nothing = null;
+
+   console.log(nothing)
+   
+
+  return (
+    <div>Datatypes</div>
+  )
+}
+
+export default Datatypes
---
+++ src/exercises/exercise1/Exercise1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Variables from './Variables'
+import Datatypes from './Datatypes'
+import Operators from './Operators'
+
+const Exercise1 = () => {
+  return (
+    <>
+    <Variables/>
+    <Datatypes/>
+    <Operators/>
+    </>
+  )
+}
+
+export default Exercise1
