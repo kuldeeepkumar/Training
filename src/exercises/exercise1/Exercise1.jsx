3d3fe344129301acbbb92ad9de74154eb3f8ff71
Ripandeep-Kaur <ripandeepk15@gmail.com>
2025-04-09 09:52:31 +0000

    exercise one is started and work in progress

---
+++ src/exercises/exercise1/Exercise1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Variables from "./Variables";
+
+const Exercise1 = () => {
+  return (
+    <>
+      <Variables />
+    </>
+  );
+};
+
+export default Exercise1;
---
+++ src/exercises/exercise1/Variables.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Variables = () => {
+  // Variable code is started here
+
+  let a = "apple";
+
+  const b = "banana";
+
+  // b = "grapes";   we can not re assign value to constant variables in java script
+
+  console.log(a);
+
+  return (
+    <div>
+      <h1> Varibles</h1>
+    </div>
+  );
+};
+
+export default Variables;
