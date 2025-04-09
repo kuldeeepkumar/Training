4a841c406d3023f6fe4598e9fceaa7b16aec1c51
simran7788762 <simrankaur7788p@gmail.com>
2025-04-09 06:08:14 +0000

    Exercise2 is done

--- src/excercise/exercise2/Loopingstatement.jsx
+++ src/excercise/exercise2/Loopingstatement.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 
 const Loopingstatement = () => {
     // code is for loop
-     for(let i=1;i<=10;i++)
+     for(let i=1;i<=25;i++)
     {
         console.log("lteration number:"+1);
     }
     //code is for while loop
 
     let i=1;
-    while(i<5){
+    while(i<10 ){
 
         console.log("lteration number:"+1)
         i++;
     }
   //code is for do while loop
-      let i = 0;
+       i = 0;
       do{
          console.log("lteration number:"+i);
          i++;
       }while (i <5);
       
+//code for foreach loop 
+ let sum = 0;
+const numbers = [56,40,23,9,10];
+ numbers.forEach(myfunction);
+
+ function myfunction(item){
+  sum +=item;
+ }
+ console.log(numbers);
 
 
   return (
