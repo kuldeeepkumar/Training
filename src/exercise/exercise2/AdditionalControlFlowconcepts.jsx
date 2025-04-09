ac0fc7a1bd71af9e15e2aa6c660ea973e827fe18
p96868673 <p96868673@gmail.com>
2025-04-09 06:38:28 +0000

    LoopingStatements is done

---
+++ src/exercise/exercise2/AdditionalControlFlowconcepts.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AdditionalControlFlowconcepts = () => {
+    for (let i=0; i<5; i++){
+        if(i===3){
+            break; // exit loop when i is 3
+        }
+        console.log(i);
+      }
+
+      //continue statement code is here
+      for (let i=0; i< 5; i++)
+      {
+        if (i===2){
+          continue; // skip when i is 2
+        }
+        console.log(i);
+      }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdditionalControlFlowconcepts
--- src/exercise/exercise2/Exercise2.jsx
+++ src/exercise/exercise2/Exercise2.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ConditionalStatement from "./ConditionalStatement";
 import LoopingStatements from "./LoopingStatements";
+import AdditionalControlFlowconcepts from "./AdditionalControlFlowconcepts";
 
 const exercise2 = () => {
   return (
     <>
       {/* <ConditionalStatement /> */}
-      <LoopingStatements />
+      {/* <LoopingStatements/> */}
+      <AdditionalControlFlowconcepts/>
     </>
   );
 };
