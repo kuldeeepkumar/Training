a9bf0daf884073485dea59c90a1d5c443484d425
bhatiapalak288 <bhatiapalak288@gmail.com>
2025-04-09 10:16:36 +0000

    exercise1 is done and exercise2 is in the progress

--- src/exercises/exercise2/Exercise2.jsx
+++ src/exercises/exercise2/Exercise2.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ConditionalStatements from "./ConditionalStatements";
+import Loops from "./Loops";
+import Questions from "./Questions";
 
 const Exercise2 = () => {
   return (
     <>
       <ConditionalStatements />
+      <Loops/>
+      <Questions/>
     </>
   );
 };
--- src/exercises/exercise2/Loops.jsx
+++ src/exercises/exercise2/Loops.jsx
@@ -1,27 +1,51 @@
 import React from 'react'
 
 const Loops = () => {
-    for(let i=1;i<=10;i++)
+    // here is the for loop 
+    for(let i=0;i<=10;i++)
     {
-       console.log(i);
+        console.log(i);
     }
-    let j=0;
-while(j<5){
-    console.log("iteration number:"+j);
-    j++;
+    for (let i = 0; i<5; i++)
+{
+    console.log("iteration number: " + i);
 }
-let k=0;
+let j = 0;
+while (j < 5)
+{
+    console.log("iteration number: " + j);
+    j++
+}
+let a = 0;
 do{
-    console.log("iteration number:"+k);
-        k++;
-    }while(k<5);
-   
-  return (
-    <div>
-          <h1>Loops</h1>
-    </div>
+    console.log("iteration number:" + a);
+    a++;
+}
+while( a < 5);
+let fruits=["apple","banana","cherry"];
+fruits.forEach(function(fruit)
+{
+    console.log(fruit);
+});
+for (let i= 0; i< 5; i++)
+{
+    if (i === 3)
+    {
+        break;// exit loop when i is 3
+    }
+    console.log(i);
+}
+for (let b =0; b < 5; b++)
+{
+    if (b == 2)
+    {
+        continue; //skip when i is 2
+    }
+    console.log(b);
+}
+        return (
+    <div>Loops</div>
   )
 }
 
-    
-    export default Loops
+export default Loops
