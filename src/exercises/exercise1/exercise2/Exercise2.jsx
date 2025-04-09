63350111f816762a70d0204db13934e5803923b7
sheetal-kaur <pinko7018@gmail.com>
2025-04-09 05:45:35 +0000

    Exercise2 is done

--- src/exercises/exercise1/exercise2/Exercise2.jsx
+++ src/exercises/exercise1/exercise2/Exercise2.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ConditionalStatements from './ConditionalStatements'
+import Loops from './Loops'
 
 const Exercise2 = () => {
   return (
+    <>
     <ConditionalStatements/>
+    <Loops/>
+    </>
   )
 }
 
