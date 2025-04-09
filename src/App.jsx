0881d525979717c0b4fd91639db0f05ba9f8ae07
preet249 <bandhanpreet872@gmail.com>
2025-04-09 05:45:53 +0000

    Exercise 2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Exercise1 from "./exercises/exercise1/Exercise1"
-import Exercise2 from "./exercises/exercise2/Exercise2"
+import Exercise1 from "./exercises/exercise1/Exercise1";
+import Exercise2 from "./exercises/exercise2/Exercise2";
 
 function App() {
-  
-
   return (
     <>
-      {/* <Exercise1/> */}
-      <Exercise2/>
+      <Exercise1 />}
+      <Exercise2 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
