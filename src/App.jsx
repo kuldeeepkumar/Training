cb14b5a9eeef51fc5ffb23d9ae5fcf1ca95e4250
sheetal-kaur <pinko7018@gmail.com>
2025-04-11 09:32:01 +0000

    Exercise3 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Exercise1 from './exercises/exercise1/Exercise1';
 import Exercise2 from './exercises/exercise1/exercise2/Exercise2';
+import Exercise3 from './exercises/exercise3/Exercise3';
 
 
 function App() 
@@ -8,8 +9,8 @@ function App()
 
   return (
     <>
-      <Exercise1/>
-      <Exercise2/>
+      <Exercise3/>
+
      
     </>
   )
---
+++ src/exercises/exercise3/Exercise3.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Exercise3 = () => {
+     //Function declarations
+   function greet(name) {
+    return `Hello, ${name}`;
+  }
+  console.log(greet("Sheetal"));
+  sayHello();
+
+  function sayHello(){
+    console.log("Hello");
+  }
+  //Arrow functions
+  const add = (a, b) => {
+    return a + b;
+  };
+
+  console.log(add(4, 7));
+  //Implicit Return
+  const square = x => x * x;
+
+  console.log(square(6));
+ 
+
+  // person.greet();
+  const person = {
+    name: "Sheetal",
+    greet() {
+      console.log(`Hello, I'm ${this.name}`);
+    }
+  };
+
+  person.greet();
+  //Parameters,Arguments
+  function myFun(name) {
+    return `Hello, ${name}`;
+  }
+  console.log(myFun("sheetal"));
+  
+function show(x, y) {
+    console.log(x,y);
+  }
+
+  show(1);
+  show(1, 2, 3);
+  //Return values
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  const result = multiply(2, 4);
+  console.log(result);
+  //Default Parameters
+  function geet(name = "Guest") {
+    return `Welcome, ${name}`;
+}
+
+console.log(geet());
+//Rest Parameters
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(sum(1, 2, 3, 4));
+//The arguments Object
+function logArgs() {
+  console.log(arguments);
+}
+
+logArgs(1, 2, 3);
+
+
+  
+  return (
+    <div>Exercise3</div>
+  )
+}
+
+export default Exercise3
