33183a9001339e035fc349ab76197bce482ace3e
jaspreet-kaur26 <jaspreett2023@gmail.com>
2025-04-11 09:25:47 +0000

    Exercise3 is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Exercise1 from './exercises/exercise1/Exercise1'
 import Exercise2 from './exercises/exercise2/Exercise2'
+import Exercise3 from './exercises/exercise3/Exercise3'
 
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
   return (
     <> 
    
-    <Exercise1/>
-     <Exercise2/>
+    {/* <Exercise1/>
+     <Exercise2/> */}
+     <Exercise3/>
 
     </>
   )
