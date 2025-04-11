f8caa01c026110fe87d94e30775853bdf015a21e
baljitkaur-badhan <baljitkaur23102006@gmail.com>
2025-04-11 09:11:49 +0000

    Exercise3 is done

---
+++ src/exercises/exercise3/Exercise3.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+const Exercise3 = () => {
+  //1
+  console.log("1 .Function declaration");
+
+  function greet(name) {
+    return ` Hello, ${name}`;
+  }
+
+  console.log(greet("Baljit"));
+
+  //2
+
+  console.log("2. function declaration");
+
+  sayHello();
+
+  function sayHello() {
+    console.log("Hi");
+  }
+
+  //3
+
+  console.log("Arrow Function");
+
+  const add = (a, b) => {
+    return a + b;
+  };
+  console.log(add(2, 3));
+
+  //4
+
+  console.log("Implicit Return");
+
+  const square = (x) => x * x;
+  console.log(square(5));
+
+  //5
+
+  console.log("No own this");
+
+  const person = {
+    name: "Baljit",
+    greet() {
+      console.log(`Hello,I'm ${this.name}`);
+    },
+  };
+
+  person.greet();
+
+  //6
+
+  console.log("Parameter and arguments");
+  //parameter
+
+  function sayhello(name) {
+    return `Hello, ${name}`;
+  }
+
+  // arguments
+
+  console.log(sayhello("Anchal0 gagan"));
+
+  //7
+
+  console.log("arguments");
+
+  function show(x, y) {
+    console.log(x, y);
+  }
+
+  console.log(show(1));
+  console.log(show(1, 2, 3));
+
+  //8 Return values
+
+  function multiply(a, b) {
+    return a * b;
+  }
+  const result = multiply(2, 4);
+  console.log("result of multiplication", result);
+
+  //9
+
+  console.log("default parameter");
+
+  function greet(name = "Guest") {
+    return ` welcome ,${name}`;
+  }
+
+  console.log(greet());
+
+  //10
+
+  console.log("rest parameter");
+
+  function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+  }
+  console.log(sum(1, 2, 3, 4));
+
+  //11
+
+  console.log("The arguments object");
+
+  function logArgs() {
+    console.log(arguments);
+  }
+  logArgs(1, 2, 3);
+
+  return <div>Exercise3</div>;
+};
+
+export default Exercise3;
