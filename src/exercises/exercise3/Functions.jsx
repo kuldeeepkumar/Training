3cb7fd65ce7289e211f975b8e54c6d42b6ab934a
arshk999 <arshdeepkalsi949@gmail.com>
2025-04-14 07:33:45 +0000

    all done

---
+++ src/exercises/exercise3/Functions.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Functions = () => {
+    //Function declaration
+    function greet(name) {
+        return `Hello, ${name}!`;
+    }
+    console.log(greet("arsh"));
+    //Arrow function
+    const square = (x) => {
+        return x * x;
+    }
+    console.log(square(4));
+
+    const person = {
+        name: "arsh",
+        age: 19,
+        greet: function() {
+            console.log(`Hello, my name is ${this.name}`);
+        }
+    }   
+    person.greet();
+
+    //parametrized function
+    function sayHello(name) {
+        return `Hello, ${name}!`;
+    }
+    console.log(sayHello("arsh"));
+    //return value
+    function add(a, b) {
+        return a + b;
+    }
+    console.log(add(5, 3));
+    //default parameter
+    function greetName(name = "Guest") {
+        return `Hello, ${name}!`;
+    }
+    console.log(greetName());
+    console.log(greetName("arsh"));
+    //rest parameter
+    function sum(...numbers) {
+        let total = 0;
+        for (let number of numbers) {
+            total += number;
+        }
+        return total;
+    }
+    console.log(sum(1, 2, 3, 4, 5));
+    // the arguments object
+    function logArgs(){
+        console.log(arguments);     
+    }
+    logArgs(1, 2, 3, 4, 5);
+  return (
+    <div>
+      <h1>Functions</h1>
+    </div>
+  )
+}
+
+export default Functions
---
+++ src/exercises/exercise4/Arrays.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Arrays = () => {
+  //array
+  const fruits = ["Apple", "Banana", "Cherry", "Dates"];
+  console.log(fruits);
+  //Pushing element at end
+  fruits.push("Mango");
+  console.log(fruits);
+  //POP element from end
+  fruits.pop();
+  console.log(fruits);
+  //Add element at start
+  fruits.unshift("Mango");
+  console.log(fruits);
+  //POP element from start
+  fruits.shift();
+  console.log(fruits);
+  //Objects
+  const person={
+    name:"arsh",
+    age:19,
+    isStudent: true
+  };
+  console.log(person.name);
+  console.log(person['age']);
+  person.age=20;
+  person.city='New York';
+   console.log(person.city);
+  //array Methods
+  //Map: Transform each item in array
+  const numbers1 = [1, 2, 3];
+  const double = numbers1.map((number) => number * 2);
+  console.log(double);
+//Filter: Filter items base on conditions
+const numbers2=[1,2,3];
+const evens=numbers2.filter((num)=>num%2==0);
+console.log(evens);
+//Reduce: Reduce array to single value
+const sum=numbers1.reduce((acc,num)=>acc+num,0);
+console.log(sum);
+  return (
+    <div>
+      <h1>Arrays</h1>
+    </div>
+  );
+}
+
+export default Arrays
