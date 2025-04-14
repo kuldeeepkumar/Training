c88757c4508265d5746f17e8c27dd7f2f868b8d4
preet249 <bandhanpreet872@gmail.com>
2025-04-14 10:20:07 +0000

    Exercise 5 is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import Exercise1 from "./exercises/exercise1/Exercise1";
 import Exercise2 from "./exercises/exercise2/Exercise2";
 import Exercise3 from "./exercises/exercise3/Exercise3";
 
+import Exercise4 from "./exercises/exercise4/Exercise4";
+import Exercise5 from "./exercises/exercise5/Exercise5";
+
 function App() {
   return (
     <>
-      {/*<Exercise1 />  */}
+      {/*<Exercise1 />   */}
       {/* <Exercise2 />  */}
-      <Exercise3 />
+      {/* <Exercise3 />  */}
+      {/*<Exercise4 />   */}
+      <Exercise5/>
+      
     </>
   );
 }
