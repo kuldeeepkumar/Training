82ef4692d81adb69db8ce681dc5b2fe31ca51858
Binni_balagan <binnibinni09@gmail.com>
2025-04-14 07:07:08 +0000

    all methods

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Exercise1 from './exercises/exercise1/Exercise1'
 import Exercise2 from './exercises/exercise2/Exercise2'
 import Questions from './exercises/Questions'
 import Exercise3 from './exercises/exercise3/Exercise3'
+import Exercise4 from './exercises/exercise4/Exercise4'
 function App() {
   return (
     <>
     <Exercise1/>
     <Exercise2/>
     <Exercise3/>
+    <Exercise4/>
     </>
   );
 }
