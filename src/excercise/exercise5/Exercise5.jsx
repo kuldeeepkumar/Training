14d219e4feff4952cdb3b1c901187c4734b0a6a3
simran7788762 <simrankaur7788p@gmail.com>
2025-04-14 10:20:02 +0000

    Exercise5 is statring

--- src/excercise/exercise5/Exercise5.jsx
+++ src/excercise/exercise5/Exercise5.jsx
@@ -5,7 +5,30 @@ const Exercise5 = () => {
     console.log(a);
     console.log(typeof a);
 
+    let car = null;
+    console.log(car);
+    console.log(typeof car);
+
+    let bike;
+    console.log(bike);
+
+    function greet(){
+        console.log("Hello!");
     
+    }
+    let result = greet();
+    console.log(result);
+ // NaN(NOT -A-NUMBER) 
+ 
+ let x = 0/0;
+ console.log(x);
+
+ let y =  parseInt("hello");
+ console.log(y);
+
+ console.log(typeof NaN);
+ console.log(NaN === NaN);
+
     
   return (
     <div>Exercise5</div>
