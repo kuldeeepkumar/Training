4648b1f9693324294165d8160c905e7691b9821f
sheetal-kaur <pinko7018@gmail.com>
2025-04-14 10:17:32 +0000

    Exercise4 is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import Exercise1 from './exercises/exercise1/Exercise1';
 import Exercise2 from './exercises/exercise1/exercise2/Exercise2';
 import Exercise3 from './exercises/exercise3/Exercise3';
+import AllMethods from './exercises/exercise4/AllMethods';
+import Exercise4 from './exercises/exercise4/Exercise4';
 
 
 function App() 
@@ -9,7 +11,10 @@ function App()
 
   return (
     <>
-      <Exercise3/>
+      {/* <Exercise3/> */}
+ {/* <Exercise4/>  */}
+ <AllMethods/>
+
 
      
     </>
