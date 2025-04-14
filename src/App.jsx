9c3ccf6cec936779d0aedb6fcfedb03d13c9c377
shikha-kashab <shikhakashab@gmail.com>
2025-04-14 10:21:35 +0000

    Exercises5 in done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Exercises2 from './exercises/exercises2/Exercises2';
 import Exercises3 from './exercises/exercises3/Exercises3';
 import Allmethods from './exercises/exercises4/Allmethods';
 import Exercises4 from './exercises/exercises4/Exercises4';
+import Exercises5 from './exercises/exercises5/Exercises5';
 
 function App() {
   
@@ -15,7 +16,8 @@ function App() {
   {/*<Exercises2/>*/}
   {/* <Exercises3/> */}
   {/* <Exercises4/> */}
-  <Allmethods/>
+  {/* <Allmethods/> */}
+  <Exercises5/>
 
 </>
   )
