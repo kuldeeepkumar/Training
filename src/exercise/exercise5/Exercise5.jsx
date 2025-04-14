5a2f3727d4255a48aa258e54f40294d7c15262d9
mehak-d940 <mehakunchibassi3@gmail.com>
2025-04-14 10:17:56 +0000

    Exercise5 is starting

--- src/exercise/exercise5/Exercise5.jsx
+++ src/exercise/exercise5/Exercise5.jsx
@@ -15,6 +15,21 @@ const Exercise5 = () => {
     }
     let result = greet();
     console.log(result);
+ // NaN(NOT -A-NUMBER) 
+ 
+ let x = 0/0;
+ console.log(x);
+
+ let y =  parseInt("hello");
+ console.log(y);
+
+ console.log(typeof NaN);
+ console.log(NaN === NaN);
+
+ //array destructuring 
+ const [var1,var2]=Array;
+
+
 
     
   return (
