086308c346e5ce8cafca286e16cde10b0946af5a
sumitchaudhxry <sumitchaudhary2m@gmail.com>
2025-04-14 05:10:51 +0000

    all changes done

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Exercise2 from './exercises/exercise2/Exercise2'
 import Question from './exercises/exercise2/Question'
 //import Exercise1 from './exercises/exercise1/Excercise1'
 //import Exercise2 from './exercises/exercise2/Exercise2'
+import Test from './exercises/exercise2/exercise3/Test';
 
 
 function App() {
 
   return (
     <>
-     <Question/>
-     
-    </>
+     <Test/>
+     <img src=""></img>
+</>     
+   
   )
 }
 export default App
