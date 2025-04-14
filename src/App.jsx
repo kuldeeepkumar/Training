a7fda689b9afb2444fb9ca442b3849f953c77ef4
nisha-barman <barmannisha63@gmail.com>
2025-04-14 04:40:53 +0000

    Exercise 3 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Exercise2 from "./exercise2/Exercise2";
 // import Exercise1 from "./exercises/exercise1/Exercise1";
-
+import Exercise3 from "./exercises/exercise3/Exercise3";
 function App() {
   return (
     <>
       {/* <Exercise1 /> */}
-      <Exercise2/>
+      {/* <Exercise2/> */}
+      <Exercise3/>
     </>
   );
 }
