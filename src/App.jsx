573ccbbe71a1659f62adf014235d0bb2f78abb71
shivani-32 <shivnikoundal@gmail.com>
2025-04-15 07:17:05 +0000

    exercises4 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 // import Exercises1 from './exercises/exercises1/Exercises1';
-import Exercise2 from './exercises/exercises2/Exercise2';
-import Exercises3 from './exercises/exercise3/Exercises3';
+// import Exercise2 from './exercises/exercises2/Exercise2';
+// import Exercises3 from './exercises/exercise3/Exercises3';
+// import Exercises4 from './exercises/exercises4/Exercises4';
+// import AllMethods from './exercises/exercises4/AllMethods';
+import Exercise5 from './exercises/exercises5/Exercise';
 
 const App = () => {
   return (
     <>
      {/* <Exercises1/> */}
     {/* <Exercise2/> */}
-    <Exercises3/>
+    {/* <Exercises3/> */}
+    {/* <Exercises4/> */}
+    {/* <AllMethods/> */}
+    <Exercise5/>
+    
 
     </>
   )
---
+++ src/exercises/exercises4/Exercises4.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Exercises4 = () => {
+    const fruits =['APPLE','Banan','Cherry'] ;
+  //push() 
+   fruits.push('mango'); 
+   console.log(fruits);
+   //pop()
+   fruits.pop();
+   console.log(fruits);
+   //shift()
+   fruits.shift();
+   console.log(fruits);
+   //unshift()
+   fruits.unshift('orange');
+   console.log(fruits);
+    
+ //objects and properties
+  const person ={
+     name:'shivani',
+     age:23,
+     isStudent:false
+  } ;
+  console.log(person.name);
+  console.log(person['age']);
+ 
+  person.age =31;
+  person.city ='new york';
+  console.log(person.city);
+ 
+  //array method
+ 
+  //map()
+  const numbers =[1,2,3];
+  const doubled =numbers.map(num =>num*2);
+  console.log(numbers);
+  console.log(doubled);
+  //filter
+  const numbers1=[1,2,3,4,5];
+  const evens =numbers1.filter(num=>num %2===0);
+  console.log(evens);
+  //reduce()
+  const numbers2 =[1,2,3,4];
+  const sum= numbers2.reduce((acc,num)=>acc+num,0);
+  console.log(sum);
+                                 
+ 
+  return (
+    <div>Exercises4</div>
+  )
+}
+
+export default Exercises4
