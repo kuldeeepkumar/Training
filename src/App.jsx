d3e8d633d596b0994c82c0fdd577ea9a13a6f5dc
Priya-Thakur04 <priyathakur144211@gmail.com>
2025-04-15 05:58:11 +0000

    exercise1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import Exercise5 from './exercises/exercise5/Exercise5'
 function App() {
   return (
     <>
-    {/* <Exercise1/>
-    <Exercise2/>
-    <Exercise3/>
-    <Exercise4/> */}
-    <Exercise5/>
+    <Exercise1/>
+    {/* <Exercise2/> */}
+    {/* <Exercise3/> */}
+    {/* <Exercise4/>  */}
+    {/* <Exercise5/> */}
     </>
   );
 }
