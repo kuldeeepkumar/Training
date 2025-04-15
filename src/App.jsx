45190beece7b015aea7c8082769f9651ef4275a6
shivani-32 <shivnikoundal@gmail.com>
2025-04-15 06:46:25 +0000

    exerices3 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 // import Exercises1 from './exercises/exercises1/Exercises1';
 import Exercise2 from './exercises/exercises2/Exercise2';
+import Exercises3 from './exercises/exercise3/Exercises3';
 
 const App = () => {
   return (
     <>
      {/* <Exercises1/> */}
-    <Exercise2/>
+    {/* <Exercise2/> */}
+    <Exercises3/>
+
     </>
   )
   
