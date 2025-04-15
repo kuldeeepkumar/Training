788b98590d824beda75524145de5bd18f5e4bf77
anchal835 <anchalkumari.191104@gmail.com>
2025-04-15 09:13:47 +0000

    exercise2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Exercise1 from "./exercises/exercise1/Exercise1";
+import Exercise2 from "./exercise2/Exercise2";
+// import Exercise1 from "./exercises/exercise1/Exercise1";
 
 function App() {
   return (
     <>
-      <Exercise1 />
+      {/* <Exercise1 //> */}
+      <Exercise2/>
     </>
   );
 }
