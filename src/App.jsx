6d5ef77beaf98ab12cd3dc1882ee6510df3d1343
tania-kaith <taniakaith2@gmail.com>
2025-04-15 06:28:01 +0000

    Exercise2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
 import React from 'react'
-import Exercise1 from './exercises/exercise1/Exercise1'
+import Exercise2 from './exercises/exercise2/Exercise2'
 
 const App = () => {
-  
   return (
-    <Exercise1/>
-   
-  )
-}
+    <>
+    {/* <Exercise1/> */}
+    <Exercise2/>
+    </>
+  );
+    }
 
-export default App
+export default App;
---
+++ src/exercises/exercise2/Exercise2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ConditionalStatement from './ConditionalStatements'
+import LoopingStatement from './LoopingStatement';
+import AdditionalControlFlowconcepts from './AdditionalControlFlowconcepts';
+import Questions from './Questions';
+
+const Exercise2 = () => {
+  return (
+    <div>
+        {/* <ConditionalStatement/> */}
+        {/* <LoopingStatement/> */}
+        {/* <AdditionalControlFlowconcepts/> */}
+        <Questions/>
+        
+    </div>
+  );
+};
+
+export default Exercise2;
