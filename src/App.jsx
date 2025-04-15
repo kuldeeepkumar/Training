5cd2c4870a86c08260c8ec635191a4ce9f9bde1d
tania-kaith <taniakaith2@gmail.com>
2025-04-15 08:58:08 +0000

    Exercise 6 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Exercise2 from './exercises/exercise2/Exercise2'
+import Exercise3 from './exercises/exercise3/Exercise3';
+import Exercise4 from './exercises/exercise4/Exercise4';
+import AllMethods from './exercises/exercise4/AllMethods';
+import Exercise5 from './exercises/exercise5/Exercise5';
+import Exercise6 from './exercises/exercise6/Exercise6';
 
 const App = () => {
   return (
     <>
     {/* <Exercise1/> */}
-    <Exercise2/>
-    </>
-  );
+    {/* <Exercise2/> */}
+    {/* <Exercise3/> */}
+    {/* <Exercise4/> */}
+    {/* <AllMethods/> */}
+    {/* <Exercise5/> */}
+    <Exercise6/>
+        </>
+  )
     }
 
 export default App;
--- src/exercises/exercise2/Questions.jsx
+++ src/exercises/exercise2/Questions.jsx
@@ -1,52 +1,6 @@
 import React from 'react'
 
 const Questions = () => {
-     //Question1
- let number = 50;
- if (number> 20) {
-     console.log("positive");
- } else if (number < 15){
-     console.log("zero");
- } else {
-     console.log("negative");
- }
- 
- //Question2
- let days=40;
- if(days>10){
-     console.log("sunday");
- }else{
- console.log("thursday");
- 
- }
- 
- //Question3
- console.log("Question3")
- for (let i =1; i<= 10;i++){
-     console.log(i, i* i);
- 
- 
- }
- 
- //Question4
- let y = 1;
- let sum = 0;
- while(y <= 50){
-  if(y % 2 ==0){
-      sum += y;
-  }
-  y++;
- }
- console.log("sum of even numbers from 1 to 50:",sum);
- //Question 5
- const numbers = [10, 23, 4, 15, 8];
-     numbers.forEach(num=>{
-      if(num % 2 === 0){
-          console.log(num,"num is even");
-      }else{
-          console.log(num ,"num is odd");
-      }
-  });
   return (
     <div>Questions</div>
   )
