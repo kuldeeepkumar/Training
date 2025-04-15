0f1623d9d6238b42877c849e306639ce58e8b41b
Amanjeet kaur <badhanaman92@gmail.com>
2025-04-15 07:29:09 +0000

    exercise 6 is done

---
+++ src/exercises/exercise 6/Exercise6.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export const Exercise6 = () => {
+  //Global scope
+  let globalvar = " I am global";
+  function test() {
+    console.log(globalvar);
+  }
+  test();
+  console.log(globalvar);
+  // Local function
+  function test() {
+    let localvar = " I am local";
+    console.log(localvar);
+  }
+  test();
+  // console.log(localvar);
+
+  //lexcial scope (static scope)
+  function outer() {
+    let outervar = " I'm from outer";
+    function inner() {
+      console.log(outervar);
+    }
+    inner();
+  }
+  outer();
+
+  //lexical scope(closures)
+  function outer() {
+    let count = 0;
+    return function inner() {
+      count++;
+      console.log("count is:", count);
+    };
+  }
+  const counter = outer();
+  counter();
+  counter();
+  counter();
+  //Real-world use case of closures
+  function makeMultiplier(factor) {
+    return function (num) {
+      return num * factor;
+    };
+  }
+  const double = makeMultiplier(2);
+  console.log(double(5));
+
+  return(
+   <div>Exercise6</div>
+  )
+};
