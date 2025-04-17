14a7cbf400c9d4d7c06551c0949fefe7bac0db03
simran7788762 <simrankaur7788p@gmail.com>
2025-04-17 06:50:15 +0000

    Exercise5 is done

--- src/excercise/exercise5/Exercise5.jsx
+++ src/excercise/exercise5/Exercise5.jsx
@@ -29,9 +29,101 @@ const Exercise5 = () => {
  console.log(typeof NaN);
  console.log(NaN === NaN);
 
+ //array Destructuring 
+
+ 
+ const colors = ['red','green','blue'];
+
+ const[first,second,third] = colors;
+
+ console.log(first);
+ console.log(second);
+ console.log(third);
+
+
+
+
+ //using rest...
+ 
+ const fruits = ['apple','mango','cherry','date'];
+ const[f1,...restfruits] = fruits;
+
+ console.log(f1);
+ console.log(restfruits);
+
+ //object Destructuring 
+
+ const person = {
+  name:'alice',
+  age:30,
+  job:'developer'
+ };
+
+ const{name,age} = person;
+
+ console.log(name);
+ console.log(age);
+
+ //renaming variables
+
+ const{name:username,age:userage} = person;
+
+ console.log(username);
+ console.log(userage);
+
+//nested Destructuring
+
+const user = {
+  id: 1,
+  profile: {
+    username:'john_doe',
+    email:'john@example.com'
+  }
+};
+
+const { profile:{email} } = user;
+console.log(email);
+
+//spread operator
+
+const arr1 = [1,2,3];
+const arr2 = [arr1,4,5];
+console.log(arr2);
+
+const obj1 = {a:1,b:2};
+const obj2={obj1,c:3};
+console.log(obj2);
+
+const num = [10,20,30];
+
+function sum(a,b,c) {
+  return a+b+c;
+
+}
+console.log(sum(num));
+
+//rest parameters
+
+const no1 = [1,2,3];
+const no2 = [no1,4,5];
+console.log(no2);
+//rest in desturcturing
+
+const[seven,rest] = [10,20,30,40];
+console.log(seven);
+console.log(rest);
+
+
+
+
+  
+
+
     
   return (
-    <div>Exercise5</div>
+    <div>
+      <h1>Exercise5</h1>
+      </div>
   )
 }
 
