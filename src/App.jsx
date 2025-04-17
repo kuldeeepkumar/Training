6ca7a279d2c51d061cf08bb3569261fc12b74672
deepodhra09 <deepodhra@gmail.com>
2025-04-17 07:53:32 +0000

    file linking with exercise2 array and all methods are done and i App js file rest exercise files are linked

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ import Exercise3 from './exercises/exercise3/Exercise3'
 import Card from './exercises/experiments/Card'
 import Arr from './exercises/exercise4/Arr'
 import Allmethods from './exercises/exercise4/Allmethods'
+import Exercise4 from './exercises/exercise4/Exercise4'
+import Exercise5 from './exercises/exercise5/Exercise5'
+import Exercise6 from './exercises/exercise6/Exercise6'
 
 
 
 function App() {
   return (
     <>
-    {/* <Exercise1/>
+   {/* <Exercise1/>
     <Exercise2/>
-    <Exercise3/> */}
-    <Arr/>
-    <Allmethods/>
+    <Exercise3/>  
+    <Exercise4/> 
+    <Exercise5/> 
+    */}
+      <Exercise6/>
     </>
   );
 }
---
+++ src/exercises/exercise4/Exercise4.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Arrays from './Arr'
+import AllMethods from './Allmethods'
+
+const Exercise4 = () => {
+  return (
+    <>
+      <Arrays/>
+      <AllMethods/>
+    </>
+  )
+}
+
+export default Exercise4
