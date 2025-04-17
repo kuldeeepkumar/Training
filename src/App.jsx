093e7d4f274d632392520ad3d68584353122da37
anchal835 <anchalkumari.191104@gmail.com>
2025-04-17 06:54:29 +0000

    AllMethod  is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import AllMethod from "./exercises/exercise4/AllMethod";
 // import Exercise2 from "./exercise2/Exercise2";
 // import Exercise3 from "./exercises/exercise3/Exercise3";
-import Exercise4 from "./exercises/exercise4/Exercise4";
+// import Exercise4 from "./exercises/exercise4/Exercise4";
 // import Exercise1 from "./exercises/exercise1/Exercise1";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       {/* <Exercise1 //> */}
       {/* <Exercise2/> */}
       {/* <Exercise3/> */}
-      <Exercise4/>
+      {/* <Exercise4/> */}
+      <AllMethod/>
     </>
   );
 }
