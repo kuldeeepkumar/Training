295a87fd8ba9ff427f384587ea27ed4c7b606a61
kauranmolpreet <anmolpreet1d1@gmail.com>
2025-06-10 04:24:19 +0000

    Exercise1 is done

---
+++ src/exercises/exercise1/Exercise1.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Operators from "./Operators";
+import Datatypes from "./Datatypes";
+import Varaiables from "./Varaiables";
+
+const Exercise1 = () => {
+  console.log("komal hi ");
+  return (
+    <>
+      <div>
+        <h1>Exercise 1</h1>
+         <Operators /> 
+        <Datatypes/> 
+        <Varaiables />
+      </div>
+    </>
+  );
+};
+
+export default Exercise1;
---
+++ src/exercises/exercise1/Varaiables.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Varaiables = () => {
+
+    
+
+        // follwing code is representing veriables
+      
+        let name = "Alice"; // Variable declared using let
+        const age = 25; // Constant variable declared using const
+        name = "Bob"; // You can reassign 'name' since it was declared with 'let'
+       //  age = 26;          // Error: Assignment to constant variable
+     
+     
+        console.log(age)
+        console.log(name)
+     
+     
+       return (
+         <div>Variables</div>
+       )
+     }
+  
+    
+      
+    
+  
+
+
+export default Varaiables
