07855971e08be2b22153f95259d3c7488da39acc
kauranmolpreet <anmolpreet1d1@gmail.com>
2025-06-10 04:52:55 +0000

    Exercise2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Exercise1 from "./exercises/exercise1/Exercise1";
+import Exercise2 from "./exercises/exercise2/Exercise2";
 
 function App() {
   return (
     <>
       <Exercise1 />
+      <Exercise2 />
     </>
   );
 }
---
+++ src/exercises/exercise2/Exercise2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ConditionalStatements from "./ConditionalStatements";
+import Loops from "./Loops";
+
+const Exercise2 = () => {
+  return (
+    <div>
+      <h1>Exercise2</h1>
+      {/* <ConditionalStatements /> */}
+      <Loops />
+    </div>
+  );
+};
+
+export default Exercise2;
