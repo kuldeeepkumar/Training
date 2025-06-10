b1e734927d09096ec620c7928c0d5cf60fb90366
Garv4647 <karankaran08320@gmail.com>
2025-06-10 08:36:38 +0000

    issue fiixed in app js fiile

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import Exercise6 from './exercises/exercise6/Exercise6'
 function App() {
   return (
     <>
-    <Exercise1/>
+    {/* <Exercise1/>
     <Exercise2/>
     <Exercise3/>
     <Exercise4/> 
-    <Exercise5/>
+    <Exercise5/> */}
     <Exercise6/>
     </>
-  );
-}
-export default App
+  );}
+
+  export default App
