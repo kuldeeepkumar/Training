447e7abcb93872b8de955b0df89654d407bcfdb7
Pardeepsingh07 <pardeepsingh6673@gmail.com>
2025-06-10 06:55:56 +0000

    Exercise6 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Exercise2 from "./exercises/exercise2/Exercise2";
 import Exercise3 from "./exercises/exercise3/Exercise3";
 import Exercise4 from "./exercises/exercise4/Exercise4";
 import Exercise5 from "./exercises/exercise5/Exercise5";
+import Exercise6 from "./exercises/exercise6/Exercise6";
 
 function App() {
   console.log("hello");
@@ -15,7 +16,8 @@ function App() {
       <Exercise3 />
       <Exercise4 />
       <Exercise5 />
-    </>
+      <Exercise6 />
+    </> 
   );
 }
 
