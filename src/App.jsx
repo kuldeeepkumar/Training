c0d56e63a07164b23d864942703aa13166528f89
sumitchaudhxry <sumitchaudhary2m@gmail.com>
2025-06-10 06:29:20 +0000

    Exercise 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@
 import './App.css'
 import Exercise1 from './exercises/exercise1/Exercise1'
 import Exercise2 from './exercises/exercise2/Exercise2'
-import Questions from './exercises/Questions'
 import Exercise3 from './exercises/exercise3/Exercise3'
-import Card from './exercises/experiments/Card'
-import Arr from './exercises/exercise4/Arr'
-import Allmethods from './exercises/exercise4/Allmethods'
-
-
-
+import Exercise4 from './exercises/exercise4/Exercise4'
+import Exercise5 from './exercises/exercise5/Exercise5'
+import Exercise6 from './exercises/exercise6/Exercise6'
+import Exercise7 from './exercises/exercise7/Exercise7'
+import Button from './experiments/buttons'
 function App() {
   return (
     <>
-    {/* <Exercise1/>
+    <Exercise1/>
     <Exercise2/>
-    <Exercise3/> */}
-    <Arr/>
-    <Allmethods/>
-    </>
+    <Exercise3/>
+    <Exercise4/> 
+    <Exercise5/>
+    <Exercise6/>
+    <Exercise7/>
+    </> 
+   
   );
 }
 export default App
