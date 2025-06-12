c0c999f847ef918559d888287a87cf4a5950d8b5
nisha-barman <barmannisha63@gmail.com>
2025-06-12 05:31:37 +0000

    file order correction is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Exercise2 from "./exercise2/Exercise2";
+import Exercise2 from "./exercises/exercise2/Exercise2";
 import Exercise1 from "./exercises/exercise1/Exercise1";
 import Exercise3 from "./exercises/exercise3/Exercise3";
 import AllMethod from "./exercises/exercise4/AllMethod";
