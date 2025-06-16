04e4a3a87ab7c9f7d5d82b9fe7dbd5da74d3d7fc
akashb999 <123816945+akashb999@users.noreply.github.com>
2025-06-16 06:14:00 +0000

    Exercise8 is done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Exercise4 from './exercises/exercise4/Exercise4'
 import Exercise5 from './exercises/exercise5/Exercise5'
 import Exercise6 from './exercises/exercise6/Exercise6'
 import Exercise7 from './exercises/exercise7/Exercise7'
+import Exercise8 from './exercises/exercise8/Exercise8'
 import Button from './experiments/buttons'
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
     <Exercise5/>
     <Exercise6/>
     <Exercise7/>
+    <Exercise8/>
     </> 
   );
 }
