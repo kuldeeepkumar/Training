c50979868f6aa63770488e424113faa3dbb222f9
jaspreet-kaur26 <jaspreett2023@gmail.com>
2025-06-16 07:05:24 +0000

    Exercise8 is done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Exercise4 from './exercises/exercise4/Exercise4'
 import Exercise5 from './exercises/exercise5/Exercise5'
 import Exercise6 from './exercises/exercise6/Exercise6'
 import Exercise7 from './exercises/exercise7/Exercise7'
+import Exercise8 from './exercises/exercise8/Exercise8';
 
 
 function App() {
@@ -22,7 +23,13 @@ function App() {
      {/* <AllMethods/> */}
      {/* <Exercise5/> */}
      {/* <Exercise6/> */}
-     <Exercise7/> 
+     {/* <Exercise7/>  */}
+     <Exercise8/>
+
+
+
+
+     
 
     </>
   )
