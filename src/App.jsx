ce55aedca95d0e967855ccef897be1565c49fca0
shivani-32 <shivnikoundal@gmail.com>
2025-06-16 07:41:25 +0000

    Exercise 8 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Exercise7 from './exercises/exercise7/Exercise7'
+import React from "react";
+// import Exercise7 from './exercises/exercise7/Exercise7'
 // import Exercises1 from './exercises/exercises1/Exercises1';
 // import Exercise2 from './exercises/exercises2/Exercise2';
 // import Exercises3 from './exercises/exercise3/Exercises3';
 // import Exercises4 from './exercises/exercises4/Exercises4';
 // import AllMethods from './exercises/exercises4/AllMethods';
-// import Exercise5 from './exericses/exericses5/Exericse5';
-// import Exercise6 from './exericses/exerices6/Exerices6';
+// import Exercise5 from './exercises/exericses5/Exericse5';
+// import Exercise6 from './exercises/exerices6/Exerices6';
+import Exercise8 from "./exercises/exercise8/Exercise8";
 
 const App = () => {
   return (
     <>
-     {/* <Exercises1/> */}
-    {/* <Exercise2/> */}
-    {/* <Exercises3/> */}
-    {/* <Exercises4/> */}
-    {/* <AllMethods/> */}
-    {/* <Exercise5/> */}
-    {/* <Exercise6/> */}
-<Exercise7/>
-
+      {/* <Exercises1/> */}
+      {/* <Exercise2/> */}
+      {/* <Exercises3/> */}
+      {/* <Exercises4/> */}
+      {/* <AllMethods/> */}
+      {/* <Exercise5/> */}
+      {/* <Exercise6/> */}
+      {/* <Exercise7/> */}
+      <Exercise8 />
     </>
-  )
-  
-}
+  );
+};
 
-export default App
+export default App;
