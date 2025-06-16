32ab1d8b1d0fa3d4d908f9d02b7c9ef7aa5783a1
baljitkaur-badhan <baljitkaur23102006@gmail.com>
2025-06-16 09:17:57 +0000

    exercise 9 is done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Exercise5 from "./exercises/exercise5/Exercise5";
 import Exercise6 from "./exercises/exercise6/Exercise6";
 import Exercise7 from "./exercises/exercise7/Exercise7";
 import Exercise8 from "./exercises/exercise8/Exercise8";
+import Exercise9 from "./exercises/exercise9/Exercise9.jsx";
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
       {/* <Exercise5 /> */}
       {/* <Exercise6 /> */}
       {/* <Exercise7 /> */}
-      <Exercise8 />
+      {/* <Exercise8 /> */}
+      <Exercise9 />
     </>
   );
 }
