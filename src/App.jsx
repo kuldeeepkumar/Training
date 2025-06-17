1b8e9a4b05b169470afedb1d1a1d8daa4be81f5d
Harpreet299 <harpreet291104@gmail.com>
2025-06-17 04:53:28 +0000

    Exercise  8 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-//import AllMethods from "./exercises/exercise4/AllMethods";
+// import AllMethods from "./exercises/exercise4/AllMethods";
 // import Exercise1 from "./exercises/exercise1/Exercise1";
-//import Exercise2 from "./exercises/exercise2/Exercise2";
-//import Exercise3 from './exercises/exercise3/Exercise3';
-//import Exercise4 from "./exercises/exercise4/Exercise4";
+// import Exercise2 from "./exercises/exercise2/Exercise2";
+// import Exercise3 from "./exercises/exercise3/Exercise3";
+// import Exercise4 from "./exercises/exercise4/Exercise4";
 // import Exercise5 from "./exercises/exercise5/Exercise5";
-//import Exercise6 from "./exercises/exercise6/Exercise6"
-import Exercise7 from "./exercises/exercise7/Exercise7";
+// import Exercise6 from "./exercises/exercise6/Exercise6";
+// import Exercise7 from "./exercises/exercise7/Exercise7";
+import Exercise8 from "./exercises/exercise8/Exercise8";
+//import  Exercise9 from "./exercises/exercise9/Exercise9;"
 function App() {
   return (
     <>
@@ -17,7 +19,9 @@ function App() {
       {/* <AllMethods/> */}
       {/* <Exercise5 /> */}
       {/* <Exercise6/> */}
-      <Exercise7 />
+      {/* <Exercise7 /> */}
+      <Exercise8 />
+      {/* <Exercise9/> */}
     </>
   );
 }
---
+++ src/exercises/exercise9/Exercise9.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exercise9 = () => {
+  return <div>Exercise9</div>;
+};
+
+export default Exercise9;
