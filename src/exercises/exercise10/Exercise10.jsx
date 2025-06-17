a235f736646b46365b2fb4bc1711d5f8fd27d66f
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-17 07:20:39 +0000

    Exercise10 is done

---
+++ src/exercises/exercise10/Exercise10.jsx
@@ -0,0 +1,20 @@
+import multiply, { add, subtract } from "./mathUtils";
+import DefaultParameters from "./DefaultParameters";
+import DestructuringExamples from "./DestructuringExamples";
+import React from "react";
+import TemplateLeterals from "./TemplateLiterals";
+
+const Exercise10 = () => {
+  console.log("Add:", add(5, 3));
+  console.log("Subtract:", subtract(5, 3));
+  console.log("Multiply:", multiply(5, 3));
+  return (
+    <>
+      <TemplateLeterals />
+      <DefaultParameters />
+      <DestructuringExamples />
+    </>
+  );
+};
+
+export default Exercise10;
