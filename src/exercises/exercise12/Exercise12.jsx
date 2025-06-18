0759b26ac485f34c92ccf06d388573be936e7e44
jaspreet-kaur26 <jaspreett2023@gmail.com>
2025-06-18 09:20:13 +0000

    Exercise12 is done

---
+++ src/exercises/exercise12/Exercise12.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Closure from './Closure';
+import Encapsulation from './Encapsulation';
+import ImmediatelyInvokedFunction from './ImmediatelyInvokedFunction';
+import PrivatePublicVariables from './PrivatePublicVariables';
+
+const Exercise12 = () => {
+  return (
+    <>
+        <Closure/>
+        <Encapsulation/>
+        <ImmediatelyInvokedFunction/>
+        <PrivatePublicVariables/>
+    </>
+  )
+}
+
+export default Exercise12
