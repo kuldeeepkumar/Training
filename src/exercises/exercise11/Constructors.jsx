9572f1e5f790bdcc88d767301fd77aac24088194
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-18 05:04:39 +0000

    Exercsie11 is done

---
+++ src/exercises/exercise11/Constructors.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Constructors = () => {
+  //============================================================Add commentMore actions
+  // Constructors and Methods
+  //============================================================
+
+  class Car {
+    constructor(brand, model) {
+      this.brand = brand;
+      this.model = model;
+    }
+
+    displayInfo() {
+      console.log(`Brand: ${this.brand}, Model: ${this.model}`);
+    }
+  }
+
+  const car1 = new Car("Toyota", "Corolla");
+  car1.displayInfo(); // Output: Brand: Toyota, Model: Corolla
+  return <div>Constructors</div>;
+};
+
+export default Constructors;
