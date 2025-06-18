31713e3c88ff73bcac664f72d6291078a2dd157e
Garv4647 <karankaran08320@gmail.com>
2025-06-18 05:24:09 +0000

    Code till exercise11

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import Exercise6 from './exercises/exercise6/Exercise6'
 import Exercise7 from './exercises/exercise7/Exercise7'
 import Exercise8 from './exercises/exercise8/Exercise8';
 import Exercise9 from './exercises/exercise9/Exercise9';
+import Exercise10 from './exercises/exercise10/Exercise10';
+import Exercise11 from './exercises/exercise11/Exercise11';
 
 function App() {
   return (
     <>
-    <Exercise1/>
+    {/* <Exercise1/>
     <Exercise2/>
     <Exercise3/>
     <Exercise4/> 
@@ -22,6 +24,8 @@ function App() {
     <Exercise7/>
     <Exercise8/>
     <Exercise9/>
+    <Exercise10/> */}
+    <Exercise11/>
     </> 
   );
 }
