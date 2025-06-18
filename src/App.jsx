8fc0d2057258a9b8e081318be8c26c8e4c16fe27
anchal835 <anchalkumari.191104@gmail.com>
2025-06-18 07:05:40 +0000

    Exercises upto 11 are done and all the issues related to files import and unwanted files code and files itself delted

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import "./App.css";
 // import AllMethod from "./exercises/exercise4/AllMethod";
 // import Exercise5 from "./exercises/exercise5/Exercise5";
-import Exercise6 from "./exercises/exercise6/Exercise6";
-// import Exercise2 from "./exercise2/Exercise2";
+// import Exercise6 from "./exercises/exercise6/Exercise6";
 // import Exercise3 from "./exercises/exercise3/Exercise3";
 // import Exercise4 from "./exercises/exercise4/Exercise4";
 // import Exercise1 from "./exercises/exercise1/Exercise1";
+import Exercise7 from "./exercises/exercise7/Exercise7";
+import Exercise8 from "./exercises/exercise8/Exercise8";
+import Exercise9 from "./exercises/exercise9/Exercise9";
+import Exercise10 from "./exercises/exercise10/Exercise10";
+import Exercise11 from "./exercises/exercise11/Exercise11";
+import Exercise2 from "./exercises/exercise2/Exercise2";
 
 function App() {
   return (
     <>
       {/* <Exercise1 //> */}
-      {/* <Exercise2/> */}
+      <Exercise2 />
       {/* <Exercise3/> */}
       {/* <Exercise4/> */}
       {/* <AllMethod/> */}
       {/* <Exercise5/> */}
-      <Exercise6/>
+      {/* <Exercise6 /> */}
+      {/* <Exercise7 />
+      <Exercise8 />
+      <Exercise9 />
+      <Exercise10 /> */}
+      <Exercise11 />
     </>
   );
 }
