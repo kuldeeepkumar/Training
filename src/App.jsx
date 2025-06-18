cca062a42de02bfaa95ab3241a88de9913877c14
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-18 09:42:07 +0000

    Exercise12 is done

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Exercise8 from "./exercises/exercise8/Exercise8";
 import Exercise9 from "./exercises/exercise9/Exercise9";
 import Exercise10 from "./exercises/exercise10/Exercise10";
 import Exercise11 from "./exercises/exercise11/Exercise11";
+import Exercise12 from "./exercises/exercise12/Exercise12";
 
 function App() {
   return (
@@ -26,7 +27,8 @@ function App() {
       {/* <Exercise8 /> */}
       {/* <Exercise9 /> */}
       {/* <Exercise10 /> */}
-      <Exercise11 />
+      {/* <Exercise11 /> */}
+      <Exercise12 />
     </>
   );
 }
---
+++ src/exercises/exercise12/Closure.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Closure = () => {
+  // =============================Add commentMore actions
+  // 1. JavaScript Closure Example
+  // =============================
+
+  function outerFunction() {
+    let outerVariable = "I am from outer scope";
+
+    function innerFunction() {
+      console.log("Closure Example:", outerVariable);
+    }
+
+    return innerFunction;
+  }
+
+  const closureExample = outerFunction();
+  closureExample(); // Output: Closure Example: I am from outer scope
+
+  return <div>Closure</div>;
+};
+
+export default Closure;
