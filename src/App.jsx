9122c47e712d96bc0c0c3d4aeee3eaee55e4734d
shikha-kashab <shikhakashab@gmail.com>
2025-06-18 04:39:52 +0000

    Exercises 11 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-
-import './App.css'
-import Exercises1 from './exercises/exercises1/Exercises1';
-import Exercises2 from './exercises/exercises2/Exercises2';
-import Exercises3 from './exercises/exercises3/Exercises3';
-import Allmethods from './exercises/exercises4/Allmethods';
-import Exercises4 from './exercises/exercises4/Exercises4';
-import Exercises5 from './exercises/exercises5/Exercises5';
-import Exercises6 from './exercises/exercises6/Exercises6';
-import Exercises7 from './exercises/exercises7/Exercises7';
-import Exercises8 from './exercises/exercises8/Exercises8';
-import Exercises9 from './exercises/exercises9/Exercises9';
-import Exercises10 from'./exercises/exercises10/Exercises10';
+import "./App.css";
+import Exercises1 from "./exercises/exercises1/Exercises1";
+import Exercises2 from "./exercises/exercises2/Exercises2";
+import Exercises3 from "./exercises/exercises3/Exercises3";
+import Allmethods from "./exercises/exercises4/Allmethods";
+import Exercises4 from "./exercises/exercises4/Exercises4";
+import Exercises5 from "./exercises/exercises5/Exercises5";
+import Exercises6 from "./exercises/exercises6/Exercises6";
+import Exercises7 from "./exercises/exercises7/Exercises7";
+import Exercises8 from "./exercises/exercises8/Exercises8";
+import Exercises9 from "./exercises/exercises9/Exercises9";
+import Exercises10 from "./exercises/exercises10/Exercises10";
+import Exercises11 from "./exercises/exercises11/Exercises11";
 function App() {
-  
-
   return (
     <>
-  {/* <Exercises1/> */}
-  {/*<Exercises2/>*/}
-  {/* <Exercises3/> */}
-  {/* <Exercises4/> */}
-  {/* <Allmethods/> */}
-  {/* <Exercises5/> */}
-  {/* <Exercises6/> */}
-  {/* <Exercises7/> */}
-  {/* <Exercises8/> */}
-  {/* <Exercises9/> */}
-  <Exercises10/>
-  </>
-  )
+      {/* <Exercises1/> */}
+      {/*<Exercises2/>*/}
+      {/* <Exercises3/> */}
+      {/* <Exercises4/> */}
+      {/* <Allmethods/> */}
+      {/* <Exercises5/> */}
+      {/* <Exercises6/> */}
+      {/* <Exercises7/> */}
+      {/* <Exercises8/> */}
+      {/* <Exercises9/> */}
+      {/* <Exercises10/> */}
+      <Exercises11 />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/exercises/exercises11/Exercises11.jsx
@@ -0,0 +1,16 @@
+import Classes from "./Classes";
+import Constructors from "./Constructors";
+import Inheritances from "./Inheritances";
+import React from "react";
+
+const Exercise11 = () => {
+  return (
+    <>
+      <Classes />
+      <Constructors />
+      <Inheritances />
+    </>
+  );
+};
+
+export default Exercise11;
