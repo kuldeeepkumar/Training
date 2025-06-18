1c5531e87fc007c62adc08e6738b9aa4dca30707
sania-sharma0 <saniasharma1727@gmail.com>
2025-06-18 03:46:46 +0000

    exercise 10 is done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Exercise4 from './exercises/exercise4/Exercise4'
 import Exercise5 from './exercises/exercise5/Exercise5'
 import Exercise6 from './exercises/exercise6/Exercise6'
 import Exercise7 from './exercises/exercise7/Exercise7'
+import Exercise8 from './exercises/exercise8/Exercise8'
+import Exercise9 from './exercises/exercise9/Exercise9'
+import Exercise10 from './exercises/exercise10/Exercise10'
 
 function App() {
 
@@ -21,7 +24,8 @@ function App() {
   {/* <Exercise6/> */}
   {/* <Exercise7/> */}
   {/* <Exercise8/> */}
-  <Exercise9/>
+  {/* <Exercise9/> */}
+  <Exercise10/>
   
 
     </>
---
+++ src/exercises/exercise10/DestructuringExamples.jsx
@@ -0,0 +1,34 @@
+ import React from "react";
+
+const DestructuringExamples = () => {
+  //==================================================
+  //Destructuring Assignment Example for object
+  //==================================================
+
+  const user = {
+    name: "John",
+    age: 30,
+    country: "India",
+  };
+
+  const { name, age, country } = user;
+
+  console.log(name); // John
+  console.log(age); // 30
+  console.log(country); // India
+
+  //==================================================
+  //Destructuring Assignment Example for Array
+  //==================================================
+
+  const colors = ["red", "green", "blue"];
+
+  const [first, second, third] = colors;
+
+  console.log(first); // red
+  console.log(second); // green
+  console.log(third); // blue
+  return <div>DestructuringExamples</div>;
+};
+
+export default DestructuringExamples;
