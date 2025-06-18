248ab6a76f2f81be857a0b46442d5ab2c881dbe1
sania-sharma0 <saniasharma1727@gmail.com>
2025-06-18 05:03:09 +0000

    exercise11 is done

---
+++ src/exercises/exercise11/Exercise11.jsx
@@ -0,0 +1,16 @@
+import Classes from "./Classes";
+import Constructors from "./Constructors";
+import Inheritance from "./Inheritance";
+import React from "react";
+
+const Exercise11 = () => {
+  return (
+    <>
+      <Classes />
+      <Constructors />
+      <Inheritance />
+    </>
+  );
+};
+
+export default Exercise11; 
