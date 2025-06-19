97f4ee2649921619b4a9a7a9d090eaf9ddad880b
arshk999 <arshdeepkalsi949@gmail.com>
2025-06-19 05:41:23 +0000

    Exercise12 is done

--- src/exercises/exercise11/Exercise11.jsx
+++ src/exercises/exercise11/Exercise11.jsx
@@ -1,15 +1,19 @@
+import React from "react";
 import Classes from "./Classes";
 import Constructors from "./Constructors";
 import Inheritance from "./Inheritance";
-import React from "react";
 
 const Exercise11 = () => {
   return (
     <>
+    <h1>
+        Exercise 11
+    </h1>
       <Classes />
       <Constructors />
       <Inheritance />
     </>
   );
 };
+
 export default Exercise11;
