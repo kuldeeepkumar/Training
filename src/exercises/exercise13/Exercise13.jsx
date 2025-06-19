0d37e6e2ba8011b3040c23d7bfa62fd63eb98806
baljitkaur-badhan <baljitkaur23102006@gmail.com>
2025-06-19 07:20:18 +0000

    Exercise 13 is done

---
+++ src/exercises/exercise13/Exercise13.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Example1 from "./Example1";
+import Example2 from "./Example2";
+import Example3 from "./Example3";
+
+const Exercise13 = () => {
+  return (
+    <div>
+      <>
+        <Example1 />
+        <Example2 />
+        <Example3 />
+      </>
+    </div>
+  );
+};
+
+export default Exercise13;
