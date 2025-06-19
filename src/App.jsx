58964d668813d677793dae5c203cdb2c7e7d6216
thakurvinesh57 <thakurvinesh57@gmail.com>
2025-06-19 07:33:24 +0000

    exercise 1 to 13 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Exercise1 from './exercises/exercise1/Exercise1'
+import Exercise2 from './exercises/exercise2/Exercise2'
+import Exercise3 from './exercises/exercise3/Exercise3'
+import Exercise4 from './exercises/exercise4/Exercise4'
+import Exercise5 from './exercises/exercise5/Exercise5'
+import Exercise6 from './exercises/exercise6/Exercise6'
+import Exercise7 from './exercises/exercise7/Exercise7'
+import Exercise8 from './exercises/exercise8/Exercise8';
+import Exercise9 from './exercises/exercise9/Exercise9';
+import Exercise10 from './exercises/exercise10/Exercise10';
+import Exercise11 from './exercises/exercise11/Exercise11';
+import Exercise12 from './exercises/exercise12/Exercise12';
+import Exercise13 from './exercises/exercise13/Exercise13';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Exercise1/>
+    <Exercise2/>
+    <Exercise3/>
+    <Exercise4/> 
+    <Exercise5/>
+    <Exercise6/>
+    <Exercise7/>
+    <Exercise8/>
+    <Exercise9/>
+    <Exercise10/>
+    <Exercise11/>
+    <Exercise12/>
+    <Exercise13/>
+    </> 
+  );
 }
-
 export default App
---
+++ src/exercises/exercise7/Callback.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const callBack = () => {
+  // Example of a callback function in JavaScript
+  function greetUser(name,callback) {
+    console.log("Hi " + name);
+    callback();
+ }
+ function showMessage() {
+    console.log("Welcome to the website!");
+ }
+ greetUser("Akash", showMessage);
+  // Example of a callback function with asynchronous operation
+  function fetchData(callback) {
+    setTimeout(() => {
+      callback('DATA IS READY');
+    }, 2000);
+  }
+  fetchData((message) => {
+    console.log(message);
+  });
+  return (
+    <div>
+      <p>Callback Example</p>
+    </div>
+  )
+}
+
+export default callBack
