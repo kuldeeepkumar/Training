470593cdc92d37bf59e5e846977aa49ec53f6e09
sheetal-kaur <pinko7018@gmail.com>
2025-06-19 06:11:10 +0000

    Exercise13 is done

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Exercise9 from './exercises/exercise9/Exercise9';
 import Exercise10 from './exercises/exercise10/Exercise10';
 import Exercise11 from './exercises/exercise11/Exercise11';
 import Exercise12 from './exercises/exercise12/Exercise12';
+import Exercise13 from './exercises/exercise13/Exercise13';
 
 
 function App() 
@@ -29,7 +30,8 @@ function App()
 {/* <Exercise9/> */}
 {/* <Exercise10/> */}
 {/* <Exercise11/> */}
-<Exercise12/>
+{/* <Exercise12/> */}
+<Exercise13/>
 
 
 
