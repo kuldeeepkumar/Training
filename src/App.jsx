073a214c200b0ded4f612ad83d7b5d4ca5f2dbbc
anchal835 <anchalkumari.191104@gmail.com>
2025-06-19 07:21:13 +0000

    Exercise13 is done

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Exercise9 from './exercises/exercise9/Exercise9';
 import Exercise10 from './exercises/exercise10/Exercise10';
 import Exercise11 from './exercises/exercise11/Exercise11';
 import Exercise12 from './exercises/exercise12/Exercise12';
+import Exercise13 from "./exercises/exercise13/Exercise13";
+
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
     <Exercise10/>
     <Exercise11/> */}
     <Exercise12/>
+    <Exercise13/>
     </> 
   );
 }
---
+++ src/exercises/exercise13/Exercise13.jsx
@@ -0,0 +1,17 @@
+import Example1 from "./Example1";
+import Example2 from "./Example2";
+import Example3 from "./Example3";
+import React from "react";
+
+const Exercise13 = () => {
+  return (
+    <>
+    <h1>Exercise13</h1>
+      <Example1 />
+      <Example2 />
+      <Example3 />
+    </>
+  );
+};
+
+export default Exercise13;
