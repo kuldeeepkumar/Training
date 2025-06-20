009ffeeac5654854b8686118b111816b20c043dd
Pardeepsingh07 <pardeepsingh6673@gmail.com>
2025-06-20 04:21:49 +0000

    Exercise12 is done

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,25 @@ import Exercise8 from "./exercises/exercise8/Exercise8";
 import Exercise9 from "./exercises/exercise9/Exercise9";
 import Exercise10 from "./exercises/exercise10/Exercise10";
 import Exercise11 from "./exercises/exercise11/Exercise11";
+import Exercise12 from "./exercises/exercise12/Exercise12";
 
 function App() {
   console.log("hello");
 
   return (
     <>
-    <Exercise1 />
+    {/* <Exercise1 />
     <Exercise2 />
     <Exercise3 />
     <Exercise4 />
-    {/* <Exercise5 />  */}
+    <Exercise5 /> 
     <Exercise6 />
     <Exercise7 />
     <Exercise8 />
     <Exercise9 />
     <Exercise10/>
-    <Exercise11/>
+    <Exercise11/> */}
+    <Exercise12/>
     </>
   );
 }
---
+++ src/exercises/exercise12/Encapsulation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Encapsulation = () => {
+  // =====================================
+  // 3. Data Encapsulation using IIFE
+  // =====================================
+
+  const counter = (function () {
+    // Private variable
+    let count = 0;
+
+    // Public methods
+    return {
+      increment: function () {
+        count++;
+        console.log("Counter incremented to:", count);
+      },
+      decrement: function () {
+        count--;
+        console.log("Counter decremented to:", count);
+      },
+      getCount: function () {
+        return count;
+      },
+    };
+  })();
+
+  counter.increment(); // Counter incremented to: 1
+  counter.increment(); // Counter incremented to: 2
+  counter.decrement(); // Counter decremented to: 1
+  console.log("Current count (public method):", counter.getCount()); // Output: 1
+  // console.log(counter.count); // Undefined: 'count' is private
+
+  return <div><h1>Encapsulation</h1></div>;
+};
+
+export default Encapsulation;
