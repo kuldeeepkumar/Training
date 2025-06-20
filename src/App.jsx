c7bd41057bb393fe132fe5638f14b67ca9d65127
tania-kaith <taniakaith2@gmail.com>
2025-06-20 06:05:49 +0000

    Todo App Experiment is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoApp from './exercises/experiments/TodoApp';
 // import Exercise2 from './exercises/exercise2/Exercise2'
 // import Exercise3 from './exercises/exercise3/Exercise3';
 // import Exercise4 from './exercises/exercise4/Exercise4';
@@ -11,7 +12,7 @@ import React from 'react'
 // import Exercise10 from './exercises/exercise10/Exercise10';
 // import Exercise11 from './exercises/exercise11/Exercise11';
 // import Exercise12 from './exercises/exercise12/Exercise12';
-import Exercise13 from './exercises/exercise13/Exercise13';
+// import Exercise13 from './exercises/exercise13/Exercise13';
 
 const App = () => {
   return (
@@ -29,7 +30,8 @@ const App = () => {
     {/* <Exercise10/> */}
     {/* <Exercise11/> */}
     {/* <Exercise12/> */}
-    <Exercise13/>
+    {/* <Exercise13/> */}
+    <TodoApp/>
 
         </>
   )
