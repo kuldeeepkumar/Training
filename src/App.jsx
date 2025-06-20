cddf43d2f76988cae249f215e9ec5da19f0f650e
gagan-kalsi <gagangagan83557@gmail.com>
2025-06-20 07:17:34 +0000

    ReactExercise1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Exercise10 from "./exercises/exercise10/Exercise10";
 import Exercise12 from "./exercises/exercise12/Exercise12";
 import Exercise13 from "./exercises/exercise13/Exercise13";
 import TodoApp from "./exercises/experiments/TodoApp";
+import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1";
 
 function App() {
   return (
@@ -32,7 +33,8 @@ function App() {
       {/* <Exercise11 /> */}
       {/*<Exercise12 /> */}
       {/* <Exercise13 />*/}
-      <TodoApp/>
+      {/*<TodoApp/>*/}
+      <ReactExercise1 />
     </>
   );
 }
