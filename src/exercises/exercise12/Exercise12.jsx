f002108a52c47c39329130ff440ec520349e98e0
ranaranjan905 <ranaranjan905@gmail.com>
2025-06-20 04:40:57 +0000

    Code till Exercise13

---
+++ src/exercises/exercise12/Exercise12.jsx
@@ -0,0 +1,19 @@
+import Closure from "./Closure";
+import Encapsulation from "./Encapsulation";
+import ImmediatelyInvokedFunction from "./ImmediatelyInvokedFunction";
+import PrivatePublicVariables from "./PrivatePublicVariables";
+import React from "react";
+
+const Exercise12 = () => {
+  return (
+    <>
+    <h1>Exercise12</h1>
+      <Closure />
+      <ImmediatelyInvokedFunction />
+      <Encapsulation />
+      <PrivatePublicVariables />
+    </>
+  );
+};
+
+export default Exercise12;
---
+++ src/exercises/exercise3/Exercise3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Functions from './Functions'
+const Exercise3 = () => {
+  return (
+ <>
+ <h1>Exercise3</h1>
+<Functions/>
+ </>
+  )
+}
+
+export default Exercise3
