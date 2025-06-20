0fbf78ac49e5b262a1edcff8409328f719c2c08e
shikha-kashab <shikhakashab@gmail.com>
2025-06-20 07:41:27 +0000

    import statements errors in app js file is fone and React exercise 1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
 import "./App.css";
-import Exercises1 from "./exercises/exercises1/Exercises1";
-import Exercises2 from "./exercises/exercises2/Exercises2";
-import Exercises3 from "./exercises/exercises3/Exercises3";
-import Allmethods from "./exercises/exercises4/Allmethods";
-import Exercises4 from "./exercises/exercises4/Exercises4";
-import Exercises5 from "./exercises/exercises5/Exercises5";
-import Exercises6 from "./exercises/exercises6/Exercises6";
-import Exercises7 from "./exercises/exercises7/Exercises7";
-import Exercises8 from "./exercises/exercises8/Exercises8";
-import Exercises9 from "./exercises/exercises9/Exercises9";
-import Exercises10 from "./exercises/exercises10/Exercises10";
-import Exercises11 from "./exercises/exercises11/Exercises11";
-import Exercises12 from "./exercises/exercises12/Exercises12";
-import Exercises13 from "./exercises/exercises13/Exercises13";
-import TodoApp from "./exercises/experiments/Todoapp";
+
+import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1";
+import Exercise1 from "./exercises/exercises1/Exercises1";
+import Exercise2 from "./exercises/exercises2/Exercises2";
+import Exercise3 from "./exercises/exercises3/Exercises3";
+import Exercise4 from "./exercises/exercises4/Exercises4";
+import Exercise5 from "./exercises/exercises5/Exercises5";
+import Exercise6 from "./exercises/exercises6/Exercises6";
+import Exercise7 from "./exercises/exercises7/Exercises7";
+import Exercise8 from "./exercises/exercises8/Exercises8";
+import Exercise9 from "./exercises/exercises9/Exercises9";
+import Exercise10 from "./exercises/exercises10/Exercises10";
+import Exercise11 from "./exercises/exercises11/Exercises11";
+import Exercise12 from "./exercises/exercises12/Exercises12";
+import Exercise13 from "./exercises/exercises13/Exercises13";
+import TodoApp from "./exercises/experiments/TodoApp";
+
 function App() {
   return (
     <>
-      {/* <Exercises1/> */}
-      {/*<Exercises2/>*/}
-      {/* <Exercises3/> */}
-      {/* <Exercises4/> */}
-      {/* <Allmethods/> */}
-      {/* <Exercises5/> */}
-      {/* <Exercises6/> */}
-      {/* <Exercises7/> */}
-      {/* <Exercises8/> */}
-      {/* <Exercises9/> */}
-      {/* <Exercises10/> */}
-      {/* <Exercises11 /> */}
-      {/* <Exercises12 /> */}
-      {/* <Exercises13 /> */}
+      {/* <Exercise1 />
+      <Exercise2 />
+      <Exercise3 />
+      <Exercise4 />
+      <Exercise5 />
+      <Exercise6 />
+      <Exercise7 />
+      <Exercise8 />
+      <Exercise9 />
+      <Exercise10 />
+      <Exercise8 />
+      <Exercise9 />
+      <Exercise10 />
+      <Exercise11 />
+      <Exercise12 />
+
       <TodoApp />
+
+      <Exercise13 /> */}
+
+      {/* From here React Exercises are going to Start */}
+
+      <ReactExercise1 />
     </>
   );
 }
