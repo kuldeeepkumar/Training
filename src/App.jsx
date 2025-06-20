b10b3372530a5bdd83a195fb2e6c1c6783a360e0
bhatiapalak288 <bhatiapalak288@gmail.com>
2025-06-20 04:52:28 +0000

    folder correction  for exercise13 is done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Exercise9 from './exercises/exercise9/Exercise9';
 import Exercise10 from './exercises/exercise10/Exercise10';
 import Exercise11 from './exercises/exercise11/Exercise11';
 import Exercise12 from './exercises/exercise12/Exercise12';
-import Exercise13 from './exercises/exercise13/exercise13/Exercise13';
+import Exercise13 from './exercises/exercise13/Exercise13';
 
 function App() {
   return (
