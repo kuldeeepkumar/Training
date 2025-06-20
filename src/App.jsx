f806dfe4dbbaf1359a9a25292079d78601c2dd6b
sheetal-kaur <pinko7018@gmail.com>
2025-06-20 05:24:04 +0000

    Todo App experiment is done

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Exercise10 from './exercises/exercise10/Exercise10';
 import Exercise11 from './exercises/exercise11/Exercise11';
 import Exercise12 from './exercises/exercise12/Exercise12';
 import Exercise13 from './exercises/exercise13/Exercise13';
+import TodoApp from './exercises/experiments/TodoApp';
 
 
 function App() 
@@ -31,7 +32,8 @@ function App()
 {/* <Exercise10/> */}
 {/* <Exercise11/> */}
 {/* <Exercise12/> */}
-<Exercise13/>
+{/* <Exercise13/> */}
+<TodoApp/>
 
 
 
