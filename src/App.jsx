5743c940a022cb6efb9853cc134ec1a5677fb451
Amanjeet kaur <badhanaman92@gmail.com>
2025-06-23 08:46:27 +0000

    file stucture corrections are done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ import Exercise11 from "./exercises/exercise11/Exercise11";
 import Exercise12 from "./exercises/exercise12/Exercise12";
 import Exercise13 from "./exercises/exercise13/Exercise13";
 import TodoApp from "./exercises/experiments/TodoApp";
-import ReactExercise1 from "./ reactExercises/ReactExercise1";
+import ReactExercise2 from "./ reactExercises/exercise2/ReactExercise2";
+import ReactExercise1 from "./ reactExercises/exercise1/ReactExercise1";
 
 function App() {
   return (
@@ -32,7 +33,9 @@ function App() {
       <Exercise12 />
       <Exercise13 /> */}
       {/* <TodoApp /> */}
-      <ReactExercise1 />
+      {/* <ReactExercise1 /> */}
+
+      <ReactExercise2 />
     </>
   );
 }
