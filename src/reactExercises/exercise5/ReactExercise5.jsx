dd03c7402ec3cf8fa2adcf694a9fc08f38cde4d3
Harpreet299 <harpreet291104@gmail.com>
2025-06-24 10:06:38 +0000

    React Exercise 5 is done

---
+++ src/reactExercises/exercise5/ReactExercise5.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FormHandling from './FormHandling'
+import FormValidation from './FormValidation'
+import ControlledComponent from './ControlledComponent'
+
+const ReactExercise5 = () => {
+  return (
+   <>
+    <FormHandling/>
+    <FormValidation/>
+    <ControlledComponent/>
+   </>
+  )
+}
+
+export default ReactExercise5
