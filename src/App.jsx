3b5c2689244834fad1c1a737776afcf6c69fc5a3
hdeep8103 <hdeep8103@gmail.com>
2025-06-24 07:28:33 +0000

    all done till react exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ import Exercise10 from './exercises/exercise10/Exercise10';
 import Exercise11 from './exercises/exercise11/Exercise11';
 import Exercise12 from './exercises/exercise12/Exercise12';
 import Exercise13 from './exercises/exercise13/Exercise13';
-
+import TodoApp from './exercises/experiments/TodoApp'
+import ReactExercise1 from "./reactExercises/exercise1/reactExercise1";
+import ReactExercise2 from './reactExercises/exercise2/ReactExercise2'
 function App() {
   return (
     <>
@@ -27,9 +29,12 @@ function App() {
     <Exercise8/>
     <Exercise9/>
     <Exercise10/>
-    <Exercise11/> */}
-    {/* <Exercise12/> */}
-    <Exercise13/>
+    <Exercise11/>
+    <Exercise12/>
+    <Exercise13/> */}
+    {/* <TodoApp/>
+    <ReactExercise1/> */}
+    <ReactExercise2/>
     </> 
   );
 }
