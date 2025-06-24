8f7f0e6c53064c9582c29e6b6e8e1bcfaeb61ae0
gagan-kalsi <gagangagan83557@gmail.com>
2025-06-24 06:38:21 +0000

    ReactExercise3 is done

---
+++ src/reactExercises/exercise3/ReactExercise3.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import EventHandling from "./EventHandling";
+import PassingArguments from "./PassingArgument";
+import ConditionalRendering from "./ConditionalRendering";
+
+const ReactExercise3 = () => {
+  return (
+    <div>
+      <EventHandling />
+      <PassingArguments />
+      <ConditionalRendering />
+    </div>
+  );
+};
+
+export default ReactExercise3;
