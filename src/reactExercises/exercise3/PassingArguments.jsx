8d4e7d322c8f95595ee24fce5ec3cabbf2b5371d
Binni_balagan <binnibinni09@gmail.com>
2025-06-24 10:19:42 +0000

    reactexercise 3,4

---
+++ src/reactExercises/exercise3/PassingArguments.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PassingArguments = () =>  {
+  // Function that accepts a custom argument
+  const greetUser = (userName) => {
+    alert(`Hello, ${userName}!`);
+  };
+
+  return (
+    <div>
+      <h2>Passing Arguments Example</h2>
+
+      {/* Passing argument using arrow function */}
+      <button onClick={() => greetUser("Kuldeep")}>Greet Kuldeep</button>
+
+      {/* Another example with a different name */}
+      <button onClick={() => greetUser("Akash")}>Greet Akash</button>
+    </div>
+  );
+}
+export default PassingArguments;
