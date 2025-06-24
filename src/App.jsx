ecd6bdc68ab02bc428c15695e1301987f5896493
Priya-Thakur04 <priyathakur144211@gmail.com>
2025-06-24 07:07:59 +0000

    ReactExercise3 is done.

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,11 @@ import ReactExercise1 from './reactExercises/exercise1/ReactExercise1';
 import Example1 from './reactExercises/exercise2/Example1'
 import Example2 from './reactExercises/exercise2/Example2'
 import ReactExercise2 from './reactExercises/exercise2/ReactExercise2'
+import ConditionalRendering from './reactExercises/exercise3/ConditionalRendering'
+import EventHandling from './reactExercises/exercise3/EventHandling'
+import PassingArguments from './reactExercises/exercise3/PassingArgument'
+import ReactExercise3 from './reactExercises/exercise3/ReactExercise3';
+import ReactExercise4 from './reactExercises/exercise4/ReactExercise4'
 
 function App() {
   return (
@@ -36,10 +41,15 @@ function App() {
     {/* <Exercise12/> */}
     {/* <Exercise13/> */}
     {/* <TodoApp/> */}
-     {/* <ReactExercise1/>  */}
-     {/* <Example1/> */}
-     {/* <Example2/> */}
-     <ReactExercise2/>
+    {/* <ReactExercise1/>  */}
+    {/* <Example1/> */}
+    {/* <Example2/> */}
+    {/* <ReactExercise2/> */}
+    {/* <ConditionalRendering/> */}
+    {/* <EventHandling/> */}
+    {/* <PassingArguments/> */}
+    {/* <ReactExercise3/> */}
+    <ReactExercise4/>
     </> 
   );
 }
---
+++ src/reactExercises/exercise3/ReactExercise3.jsx
@@ -0,0 +1,17 @@
+import ConditionalRendering from "./ConditionalRendering";
+import EventHandling from "./EventHandling";
+
+import React from "react";
+import PassingArguments from './PassingArgument';
+
+const ReactExercise3 = () => {
+  return (
+    <div>
+      <EventHandling />
+      <PassingArguments />
+      <ConditionalRendering />
+    </div>
+  );
+};
+
+export default ReactExercise3;
