bba43f4cc38bfbdbd048a6162aa96970630f8845
p96868673 <p96868673@gmail.com>
2025-06-24 05:00:28 +0000

    React Exercise2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import TodoApp from "./exercise/experiments/TodoApp";
 import ReactExercise1 from "./ReactExercise/exercise1/ReactExercise1";
+import ReactExercise2 from "./ReactExercise/exercise2/ReactExercise2";
 // import AllMethods from "./exercise/exercise4/AllMethods";
 // import Exercise1 from "./exercise/exercise1/Exercise1";
 //import Exercise2 from "./exercise/exercise2/Exercise2";
@@ -40,7 +41,8 @@ function App() {
       {/* <Exercise12/> */}
       {/* <Exercise13/> */}
       {/* <TodoApp/> */}
-      <ReactExercise1/>
+      {/* <ReactExercise1/> */}
+      <ReactExercise2/>
     
       </>
 
