da4f44551ee7ad236f0782a40265283e006dc2c3
baljitkaur-badhan <baljitkaur23102006@gmail.com>
2025-06-24 10:05:43 +0000

    React Exercise 5 is done

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1.jsx";
 import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2.jsx";
 import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3.jsx";
 import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4.jsx";
+import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5.jsx";
 
 function App() {
   return (
@@ -41,7 +42,8 @@ function App() {
       {/* <ReactExercise1 /> */}
       {/* <ReactExercise2 /> */}
       {/* <ReactExercise3 /> */}
-      <ReactExercise4 />
+      {/* <ReactExercise4 /> */}
+      <ReactExercise5 />
     </>
   );
 }
