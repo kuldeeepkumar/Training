0878a28936c8e024634b2b94e8864aecbbbabdcd
mehak350 <mehakbhatia002@gmail.com>
2025-06-25 06:16:27 +0000

    react exercise 5

---
+++ src/reactExercises/ReactExercise5.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import FormValidation from "./exercise5/FormValidation";
+import ControlledComponent from "./exercise5/Controlledcomponent";
+import FormHandling from "./exercise5/FormHandling";
+
+const ReactExercise5 = () => {
+  return (
+    <>
+      <ControlledComponent />
+      <FormHandling />
+      <FormValidation />
+    </>
+  );
+};
+
+export default ReactExercise5;
