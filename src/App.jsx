40c1a797f148f6cb795bdc966ce71bb233bd7036
thakurvinesh57 <thakurvinesh57@gmail.com>
2025-06-25 05:22:43 +0000

    ReactExercise5 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
+import "./App.css";
+import Exercise1 from "./exercises/exercise1/Exercise1";
+import Exercise2 from "./exercises/exercise2/Exercise2";
+import Exercise3 from "./exercises/exercise3/Exercise3";
+import Exercise4 from "./exercises/exercise4/Exercise4";
+import Exercise5 from "./exercises/exercise5/Exercise5";
+import Exercise6 from "./exercises/exercise6/Exercise6";
+import Exercise7 from "./exercises/exercise7/Exercise7";
+import Exercise8 from "./exercises/exercise8/Exercise8";
+import Exercise9 from "./exercises/exercise9/Exercise9";
+import Exercise10 from "./exercises/exercise10/Exercise10";
+import Exercise11 from "./exercises/exercise11/Exercise11";
+import Exercise12 from "./exercises/exercise12/Exercise12";
+import Exercise13 from "./exercises/exercise13/Exercise13";
+import TodoApp from "./experiments/Todo";
+import ReactExercise1 from "./reactExercises/exercise 1/ReactExercise1";
+import Example1 from "./reactExercises/exercise 2/Example1";
+import Example2 from "./reactExercises/exercise 2/Example2";
+import ReactExercise2 from "./reactExercises/exercise 2/ReactExercise2";
 
-import './App.css'
-import Exercise1 from './exercises/exercise1/Exercise1'
-import Exercise2 from './exercises/exercise2/Exercise2'
-import Exercise3 from './exercises/exercise3/Exercise3'
-import Exercise4 from './exercises/exercise4/Exercise4'
-import Exercise5 from './exercises/exercise5/Exercise5'
-import Exercise6 from './exercises/exercise6/Exercise6'
-import Exercise7 from './exercises/exercise7/Exercise7'
-import Exercise8 from './exercises/exercise8/Exercise8';
-import Exercise9 from './exercises/exercise9/Exercise9';
-import Exercise10 from './exercises/exercise10/Exercise10';
-import Exercise11 from './exercises/exercise11/Exercise11';
-import Exercise12 from './exercises/exercise12/Exercise12';
-import Exercise13 from './exercises/exercise13/Exercise13';
-import TodoApp from './experiments/Todo'
-import ReactExercise1 from './reactExercises/exercise 1/ReactExercise1'
-import Example1 from './reactExercises/exercise 2/Example1'
-import Example2 from './reactExercises/exercise 2/Example2'
-import ReactExercise2 from './reactExercises/exercise 2/ReactExercise2'
+import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3";
+import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
+import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
 
 function App() {
   return (
     <>
-    {/* <Exercise1/>
+      {/* <Exercise1/>
     <Exercise2/>
     <Exercise3/>
     <Exercise4/> 
@@ -35,12 +38,17 @@ function App() {
     <Exercise11/>
     <Exercise12/>
     <Exercise13/> */}
-    {/* <TodoApp/> */}
-    {/* <ReactExercise1/> */}
-    {/* <Example1/> */}
-    {/* <Example2/> */}
-    <ReactExercise2/>
+      {/* <TodoApp/> */}
+      {/* <ReactExercise1/> */}
+      {/* <Example1/> */}
+      {/* <Example2/> */}
+      {/* <ReactExercise2/> */}
+
+      <ReactExercise3 />
+      <ReactExercise4 />
+
+      <ReactExercise5 />
     </>
   );
 }
-export default App
+export default App;
