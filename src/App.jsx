4286aa8c605644bd47f6ab389b702dd82c06b714
Muskan369 <muskanansari49399@gmail.com>
2025-06-25 10:12:16 +0000

    React Exercise 6 and 7 is  Done

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2";
 import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3";
 import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
 import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
+import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
+import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7";
 
 
 function App() {
@@ -38,7 +40,9 @@ function App() {
     {/* <ReactExercise2/> */}
     {/* <ReactExercise3/> */}
     {/* <ReactExercise4/> */}
-    <ReactExercise5/>
+    {/* <ReactExercise5/> */}
+    {/* <ReactExercise6/> */}
+    <ReactExercise7/>
     </>
   );
 }
