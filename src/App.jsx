146c58dee01c4889ca5e1e7b088a87de2c38d8a1
kuldeeepkumar <pro.kuldeepkumar@gmail.com>
2025-06-25 11:31:27 +0530

    React Exercise 6 is done

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2";
 import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3";
 import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
 import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
+import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
 import TodoApp from "./exercises/experiments/TodoApp";
 
 function App() {
@@ -48,7 +49,8 @@ function App() {
       {/* <ReactExercise2 /> */}
       {/* <ReactExercise3 />  */}
       {/* <ReactExercise4 /> */}
-      <ReactExercise5 />
+      {/* <ReactExercise5 /> */}
+      <ReactExercise6 />
     </>
   );
 }
