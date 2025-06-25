b73704090512be9602619c34fc5d2857de704d4c
p96868673 <p96868673@gmail.com>
2025-06-25 05:24:08 +0000

    one import statement issue  is fixed in App jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import TodoApp from "./exercise/experiments/TodoApp";
-import ReactExercise4 from "./ReactExercise/exercise1/exercise4/ReactExercise4";
 import ReactExercise1 from "./ReactExercise/exercise1/ReactExercise1";
 import ReactExercise2 from "./ReactExercise/exercise2/ReactExercise2";
 import ReactExercise3 from "./ReactExercise/exercise3/ReactExercise3";
@@ -9,7 +8,6 @@ import ReactExercise5 from "./ReactExercise/exercise5/ReactExercise5";
 // import Exercise1 from "./exercise/exercise1/Exercise1";
 //import Exercise2 from "./exercise/exercise2/Exercise2";
 // import Exercise3 from "./exercise/exercise3/Exercise3";
-// import Exercise4 from "./exercise/exercise4/Exercise4";
 //import Exercise5 from "./exercise/exercise5/Exercise5";
 // import Exercise6 from "./exercise/exercise6/Exercise6";
 // import Exercise7 from "./exercise/exercise7/Exercise7";
@@ -19,6 +17,7 @@ import ReactExercise5 from "./ReactExercise/exercise5/ReactExercise5";
 // import Exercise11 from './exercise/exercise11/Exercise11';
 // import Exercise12 from './exercise/Exercise12/Exercise12';
 // import Exercise13 from './exercise/exercise13/Exercise13';
+import ReactExercise4 from './ReactExercise/exercise4/ReactExercise4';
 
 
 
@@ -47,7 +46,7 @@ function App() {
       {/* <ReactExercise1/> */}
       {/* <ReactExercise2/> */} 
       {/* <ReactExercise3/> */}
-      {/* <ReactExercise4/> */}
+      <ReactExercise4/>
       <ReactExercise5/>
     
       </>
