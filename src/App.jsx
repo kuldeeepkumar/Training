7a6c761723614a9a5328fde4efc35563f56b102c
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-25 04:00:08 +0000

    ReactExercise4 is done

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import TodoApp from "./exercises/experiments/TodoApp.jsx";
 import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1.jsx";
 import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2.jsx";
 import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3.jsx";
+import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4.jsx";
+import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5.jsx";
 
 function App() {
   return (
@@ -38,7 +40,9 @@ function App() {
       {/* <TodoApp/> */}
       {/* <ReactExercise1 /> */}
       {/* <ReactExercise2 /> */}
-      <ReactExercise3 />
+      {/* <ReactExercise3 /> */}
+      {/* <ReactExercise4 /> */}
+      <ReactExercise5 />
     </>
   );
 }
