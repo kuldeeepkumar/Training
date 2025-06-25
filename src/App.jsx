b500bb8a4f4491bf692296a917dbd8b126740c2d
anchal835 <anchalkumari.191104@gmail.com>
2025-06-25 06:36:18 +0000

    ReactExercise 2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import "./App.css";
-import Exercise1 from "./exercises/exercise1/Exercise1";
-import Exercise2 from "./exercises/exercise2/Exercise2";
-import Exercise3 from "./exercises/exercise3/Exercise3";
-import Exercise4 from "./exercises/exercise4/Exercise4";
-import Exercise5 from "./exercises/exercise5/Exercise5";
-import Exercise6 from "./exercises/exercise6/Exercise6";
-import Exercise7 from "./exercises/exercise7/Exercise7";
-import Exercise8 from "./exercises/exercise8/Exercise8";
-import Exercise9 from "./exercises/exercise9/Exercise9";
-import Exercise10 from "./exercises/exercise10/Exercise10";
-import Exercise11 from "./exercises/exercise11/Exercise11";
-import Exercise12 from "./exercises/exercise12/Exercise12";
-import Exercise13 from "./exercises/exercise13/Exercise13";
-import TodoApp from "./exercises/experiment/TodoApp";
+// import Exercise1 from "./exercises/exercise1/Exercise1";
+// import Exercise2 from "./exercises/exercise2/Exercise2";
+// import Exercise3 from "./exercises/exercise3/Exercise3";
+// import Exercise4 from "./exercises/exercise4/Exercise4";
+// import Exercise5 from "./exercises/exercise5/Exercise5";
+// import Exercise6 from "./exercises/exercise6/Exercise6";
+// import Exercise7 from "./exercises/exercise7/Exercise7";
+// import Exercise8 from "./exercises/exercise8/Exercise8";
+// import Exercise9 from "./exercises/exercise9/Exercise9";
+// import Exercise10 from "./exercises/exercise10/Exercise10";
+// import Exercise11 from "./exercises/exercise11/Exercise11";
+// import Exercise12 from "./exercises/exercise12/Exercise12";
+// import Exercise13 from "./exercises/exercise13/Exercise13";
+// import TodoApp from "./exercises/experiment/TodoApp";
 import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1";
+import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2";
 
 function App() {
   return (
@@ -34,7 +35,8 @@ function App() {
       {/* <TodoApp /> */}
 
       {/* From here React Exercises are going to Start */}
-      <ReactExercise1 />
+      {/* <ReactExercise1 /> */}
+      <ReactExercise2/>
       
     </>
   );
