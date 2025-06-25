0db7e078c9b909363fba270a4ac1de72a96fe918
nisha-barman <barmannisha63@gmail.com>
2025-06-25 06:25:43 +0000

    ReactExercise 5 is done

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import ReactExercise1 from './ReactExercises/exercise1/ReactExercise1';
 import ReactExercise2 from './ReactExercises/exercise2/ReactExercise2';
 import ReactExercise3 from './ReactExercises/exercise3/ReactExercise3';
 import ReactExercise4 from './ReactExercises/exercise4/ReactExercise4';
+import ReactExercise5 from './ReactExercises/exercise5/ReactExercise5';
 
 
 
@@ -43,7 +44,8 @@ function App() {
     {/* <ReactExercise1/> */}
     {/* <ReactExercise2/> */}
     {/* <ReactExercise3/> */}
-    <ReactExercise4/>
+    {/* <ReactExercise4/> */}
+    <ReactExercise5/>
     </> 
   );
 }
