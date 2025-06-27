402ce70fce3468b54bc8c14a4111911d88cb33e9
thakurvinesh57 <thakurvinesh57@gmail.com>
2025-06-27 05:43:56 +0000

    ReactExercise8 is done

---
+++ src/reactExercises/exercise8/About.jsx
@@ -0,0 +1,14 @@
+// About.js
+
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the About page.</p>
+    </div>
+  );
+}
+
+export default About;
