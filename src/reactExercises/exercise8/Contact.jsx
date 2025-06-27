9df56e1d1f900e00dbae8edcee03417331118a6b
gagan-kalsi <gagangagan83557@gmail.com>
2025-06-27 06:13:46 +0000

    React Exercise8 is done

---
+++ src/reactExercises/exercise8/Contact.jsx
@@ -0,0 +1,14 @@
+// Contact.js
+
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Get in touch through our Contact page.</p>
+    </div>
+  );
+}
+
+export default Contact;
