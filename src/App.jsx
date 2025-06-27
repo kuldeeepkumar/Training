69076b6db48b0a972b211fd988b8e5ecbee124db
sheetal-kaur <pinko7018@gmail.com>
2025-06-27 09:17:51 +0000

    Expeeriment home page is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import Exercise1 from "./exercises/exercise1/Exercise1";
 import Exercise2 from "./exercises/exercise1/exercise2/Exercise2";
 import Exercise3 from "./exercises/exercise3/Exercise3";
@@ -22,32 +24,127 @@ import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
 import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7";
 import ReactExercise8 from "./reactExercises/exercise8/ReactExercise8";
 
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+
+
+     
+
 function App() {
-  return (
+  const javaScriptExercisesData = [
+    {
+      routesName: "/",
+      component: <HomePage/>,
+    },
+    {
+      routesName: "Exercise1",
+      component: <Exercise1 />,
+    },
+    {
+      routesName: "Exercise2",
+      component: <Exercise2 />,
+    },
+    {
+      routesName: "Exercise3",
+      component: <Exercise3 />,
+    },
+    {
+      routesName: "Exercise4",
+      component: <Exercise4 />,
+    },
+    {
+      routesName: "Exercise5",
+      component: <Exercise5 />,
+    },
+    {
+      routesName: "Exercise6",
+      component: <Exercise6 />,
+    },
+    {
+      routesName: "Exercise7",
+      component: <Exercise7 />,
+    },
+    {
+      routesName: "Exercise8",
+      component: <Exercise8 />,
+    },
+    {
+      routesName: "Exercise9",
+      component: <Exercise9 />,
+    },
+    {
+      routesName: "Exercise10",
+      component: <Exercise10 />,
+    },
+    {
+      routesName: "Exercise11",
+      component: <Exercise11 />,
+    },
+    {
+      routesName: "Exercise12",
+      component: <Exercise12 />,
+    },
+    {
+      routesName: "Exercise13",
+      component: <Exercise13 />,
+    },
+  ];
+
+  const reactExercisesData = [
+    {
+      routesName: "ReactExercise1",
+      component: <ReactExercise1 />,
+    },
+    {
+      routesName: "ReactExercise2",
+      component: <ReactExercise2 />,
+    },
+    {
+      routesName: "ReactExercise3",
+      component: <ReactExercise3 />,
+    },
+    {
+      routesName: "ReactExercise4",
+      component: <ReactExercise4 />,
+    },
+    {
+      routesName: "ReactExercise5",
+      component: <ReactExercise5 />,
+    },
+    {
+      routesName: "ReactExercise6",
+      component: <ReactExercise6 />,
+    },
+    {
+      routesName: "ReactExercise7",
+      component: <ReactExercise7 />,
+    },
+    {
+      routesName: "ReactExercise8",
+      component: <ReactExercise8 />,
+    },
+  ];
+  return(
     <>
-      {/* <Exercise3/> */}
-      {/* <Exercise4/>  */}
-      {/* <AllMethods/> */}
-      {/* <Exercise5/> */}
-      {/* <Exercise6/> */}
-      {/* <Exercise7/> */}
-      {/* <Exercise8/> */}
-      {/* <Exercise9/> */}
-      {/* <Exercise10/> */}
-      {/* <Exercise11/> */}
-      {/* <Exercise12/> */}
-      {/* <Exercise13/> */}
-      {/* <TodoApp/> */}
-      {/* <ReactExercise1/> */}
-      {/* <ReactExercise2 /> */}
-      {/* <ReactExercise3/> */}
-      {/* <ReactExercise4/> */}
-      {/* <ReactExercise5/> */}
-      {/* <ReactExercise6/> */}
-      {/* <ReactExercise7/> */}
-      <ReactExercise8/>
+    <Routes>
+        {javaScriptExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+        {reactExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+      </Routes>
     </>
   );
 }
+    
 
 export default App;
--- src/reactExercises/exercise8/ReactExercise8.jsx
+++ src/reactExercises/exercise8/ReactExercise8.jsx
@@ -7,12 +7,12 @@ import NotFound from "./NotFound";
 
 function ReactExercise8() {
   return (
-    <BrowserRouter>
+    
       <div>
 
          {/* Navigation Links */}
         <nav>
-          <Link to="/" style={{ marginRight: "10px" }}>
+          <Link to="/home" style={{ marginRight: "10px" }}>
             Home
           </Link>
           <Link to="/about" style={{ marginRight: "10px" }}>
@@ -22,7 +22,7 @@ function ReactExercise8() {
         </nav>
         {/* Route Definitions */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
 
@@ -33,7 +33,7 @@ function ReactExercise8() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    
   );
 }
 
