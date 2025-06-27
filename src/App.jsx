38545ca40a28578384f90d4ae6fa32cda16250db
Pardeepsingh07 <pardeepsingh6673@gmail.com>
2025-06-27 04:38:23 +0000

    ReactExersice7 is done

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import ReactExercise3 from "./reactExercises/exercise3/ReactExercise3";
 import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
 import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
 import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
+import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7";
 
 
 function App() {
@@ -46,6 +47,7 @@ function App() {
     <ReactExercise4/>
     <ReactExercise5/> */}
     <ReactExercise6/>
+    <ReactExercise7/>
     </>
   );
 }
