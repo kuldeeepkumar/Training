97f5c3d367beaf9ef28c2828b6dde2712580cdde
hdeep8103 <hdeep8103@gmail.com>
2025-06-27 06:33:32 +0000

    Code till reactExercise8

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ import Exercise13 from './exercises/exercise13/Exercise13';
 import TodoApp from './exercises/experiments/TodoApp'
 import ReactExercise1 from "./reactExercises/exercise1/reactExercise1";
 import ReactExercise2 from './reactExercises/exercise2/ReactExercise2'
+import ReactExercise3 from './reactExercises/exercise3/ReactExercise3'
+import ReactExercise4 from './reactExercises/exercise4/ReactExercise4'
+import ReactExercise5 from './reactExercises/exercise5/ReactExercise5'
+import ReactExercise6 from './reactExercises/exercise6/ReactExercise6'
+import ReactExercise7 from './reactExercises/exercise7/ReactExercise7'
+import ReactExercise8 from './reactExercises/exercise8/ReactExercise8'
 function App() {
   return (
     <>
@@ -31,10 +37,16 @@ function App() {
     <Exercise10/>
     <Exercise11/>
     <Exercise12/>
-    <Exercise13/> */}
-    {/* <TodoApp/>
-    <ReactExercise1/> */}
+    <Exercise13/>
+    <TodoApp/>
+    <ReactExercise1/>
     <ReactExercise2/>
+     <ReactExercise3/>
+    <ReactExercise4/>
+    <ReactExercise5/> */}
+    {/* <ReactExercise6/>
+    <ReactExercise7/> */}
+    <ReactExercise8/>
     </> 
   );
 }
---
+++ src/reactExercises/exercise5/ReactExercise5.jsx
@@ -0,0 +1,16 @@
+import ControlledComponent from "./ControlledComponent";
+import FormHandling from "./FormHandling";
+import FormValidation from "./FormValidation";
+import React from "react";
+
+const ReactExercise5 = () => {
+  return (
+    <>
+      {/* <ControlledComponent /> */}
+      {/* <FormHandling /> */}
+
+      <FormValidation />
+    </>
+  );
+};
+export default ReactExercise5;
