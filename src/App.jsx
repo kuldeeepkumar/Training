3af6a8bf7689ac9d398e0c2a9bd25a51db2167b3
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-27 07:35:24 +0000

    HomePage Experiment is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
+
 import Exercise1 from "./exercises/exercise1/Exercise1";
 import Exercise2 from "./exercises/exercise2/Exercise2";
 import Exercise3 from "./exercises/exercise3/Exercise3";
-import AllMethods from "./exercises/exercise4/AllMethods";
 import Exercise4 from "./exercises/exercise4/Exercise4";
 import Exercise5 from "./exercises/exercise5/Exercise5";
 import Exercise6 from "./exercises/exercise6/Exercise6";
@@ -21,34 +21,123 @@ import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4.jsx";
 import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5.jsx";
 import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6.jsx";
 import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7.jsx";
+import ReactExercise8 from "./reactExercises/exercise8/ReactExercise8.jsx";
+import HomePage from "./HomePage.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const javaScriptExercisesData = [
+    {
+      routesName: "/",
+      component: <HomePage />,
+    },
+    {
+      routesName: "Exercise1",
+      component: <Exercise1 />,
+    },
+    {
+      routesName: "Exercise2",
+      component: <Exercise2 />,
+    },
+    {
+      routesName: "Exercise3",
+      component: <Exercise3 />,
+    },
+    {
+      routesName: "Exercise4",
+      component: <Exercise4 />,
+    },
+    {
+      routesName: "Exercise5",
+      component: <Exercise5 />,
+    },
+    {
+      routesName: "Exercise6",
+      component: <Exercise6 />,
+    },
+    {
+      routesName: "Exercise7",
+      component: <Exercise7 />,
+    },
+    {
+      routesName: "Exercise8",
+      component: <Exercise8 />,
+    },
+    {
+      routesName: "Exercise9",
+      component: <Exercise9 />,
+    },
+    {
+      routesName: "Exercise10",
+      component: <Exercise10 />,
+    },
+    {
+      routesName: "Exercise11",
+      component: <Exercise11 />,
+    },
+    {
+      routesName: "Exercise12",
+      component: <Exercise12 />,
+    },
+    {
+      routesName: "Exercise13",
+      component: <Exercise13 />,
+    },
+  ];
+
+  const reactExercisesData = [
+    {
+      routesName: "ReactExercise1",
+      component: <ReactExercise1 />,
+    },
+    {
+      routesName: "ReactExercise2",
+      component: <ReactExercise2 />,
+    },
+    {
+      routesName: "ReactExercise3",
+      component: <ReactExercise3 />,
+    },
+    {
+      routesName: "ReactExercise4",
+      component: <ReactExercise4 />,
+    },
+    {
+      routesName: "ReactExercise5",
+      component: <ReactExercise5 />,
+    },
+    {
+      routesName: "ReactExercise6",
+      component: <ReactExercise6 />,
+    },
+    {
+      routesName: "ReactExercise7",
+      component: <ReactExercise7 />,
+    },
+    {
+      routesName: "ReactExercise8",
+      component: <ReactExercise8 />,
+    },
+  ];
   return (
     <>
-      {/* <Exercise1 />  */}
-      {/* <Exercise2 /> */}
-      {/* <Exercise3 /> */}
-      {/* <Exercise4 />  */}
-      {/* <AllMethods /> */}
-      {/* <Exercise5 /> */}
-      {/* <Exercise6 /> */}
-      {/* <Exercise7 /> */}
-      {/* <Exercise8 /> */}
-      {/* <Exercise9 /> */}
-      {/* <Exercise10 /> */}
-      {/* <Exercise11 /> */}
-      {/* <Exercise12 /> */}
-      {/* <Exercise13 /> */}
-      {/* <TodoApp/> */}
-      {/* <ReactExercise1 /> */}
-      {/* <ReactExercise2 /> */}
-      {/* <ReactExercise3 /> */}
-      {/* <ReactExercise4 /> */}
-      {/* <ReactExercise5 /> */}
-      {/* <ReactExercise6 /> */}
-      <ReactExercise7 />
+      <Routes>
+        {javaScriptExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+        {reactExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+      </Routes>
     </>
   );
 }
-
 export default App;
--- src/reactExercises/exercise8/ReactExercise8.jsx
+++ src/reactExercises/exercise8/ReactExercise8.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 import About from "./About";
 import Contact from "./Contact";
@@ -8,33 +8,31 @@ import React from "react";
 
 function ReactExercise8() {
   return (
-    <BrowserRouter>
-      <div>
-        {/* Navigation Links */}
-        <nav>
-          <Link to="/" style={{ marginRight: "10px" }}>
-            Home
-          </Link>
-          <Link to="/about" style={{ marginRight: "10px" }}>
-            About
-          </Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
+    <div>
+      {/* Navigation Links */}
+      <nav>
+        <Link to="/home" style={{ marginRight: "10px" }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ marginRight: "10px" }}>
+          About
+        </Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
 
-        {/* Route Definitions */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+      {/* Route Definitions */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
 
-          {/* Redirect example: redirecting /info to /about */}
-          <Route path="/info" element={<Navigate to="/about" replace />} />
+        {/* Redirect example: redirecting /info to /about */}
+        <Route path="/info" element={<Navigate to="/about" replace />} />
 
-          {/* Fallback route for undefined paths */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+        {/* Fallback route for undefined paths */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
