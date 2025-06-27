fd14aa6ee7be3e44bf4faa1083344abda3a7d657
anchal-thakur644 <anchalt2345@gmail.com>
2025-06-27 06:14:05 +0000

    ReactExercise8 is done

---
+++ src/reactExercises/exercise8/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Get in touch through our Contact Page.</p>
+    </div>
+  );
+}
+
+export default Contact;
