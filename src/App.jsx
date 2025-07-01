a395b17cd7c7ab236865b2336a23a32687d6b613
Ripandeep-Kaur <ripandeepk15@gmail.com>
2025-07-01 08:57:37 +0000

    tialwind css instalation is done , home page exerpiment done , exercise 9 is also done

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,171 @@
 import "./App.css";
+
+import { Link, Route, Router, Routes } from "react-router-dom";
+
 import Exercise1 from "./exercises/exercise1/Exercise1";
-import Exercise2 from "./exercises/exercise2/Exercise2";
-import Exercise3 from "./exercises/exercise3/Exercise3";
-import Exercise4 from "./exercises/exercise4/Exercise4";
-import Exercise5 from "./exercises/exercise5/Exercise5";
+import Exercise10 from "./exercises/exercise10/Exercise10";
+import Exercise11 from "./exercises/exercise11/Exercise11";
 import Exercise6 from "./exercises/exercise6/Exercise6";
 import Exercise7 from "./exercises/exercise7/Exercise7";
 import Exercise8 from "./exercises/exercise8/Exercise8";
 import Exercise9 from "./exercises/exercise9/Exercise9";
-import Exercise10 from "./exercises/exercise10/Exercise10";
-import Exercise11 from "./exercises/exercise11/Exercise11";
-import Exercise12 from "./exercises/exercise12/Exercise12";
-import Exercise13 from "./exercises/exercise13/Exercise13";
-import TodoApp from "./experiments/TodoApp";
+
 import ReactExercise1 from "./reactExercises/exercise1/ReactExercise1";
-import Example1 from "./reactExercises/exercise2/Example1";
-import Example2 from "./reactExercises/exercise2/Example2";
 import ReactExercise2 from "./reactExercises/exercise2/ReactExercise2";
-import ConditionalRendering from "./reactExercises/exercise3/ConditionalRendering";
-import EventHandling from "./reactExercises/exercise3/EventHandling";
-import PassingArguments from "./reactExercises/exercise3/PassingArgument";
-import ReactExercise3 from "./reactExercises/exercise3/ReactExercixe3";
-import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
-import ControlledComponent from "./reactExercises/exercise5/ControlledComponent";
-import FormHandling from "./reactExercises/exercise5/FormHandling";
-import FormValidation from "./reactExercises/exercise5/FormValidation";
-import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
-import MountExample from "./reactExercises/exercise6/MountExample";
-import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
-import UnmountExample from "./reactExercises/exercise6/UnmountExample";
-import UpdateExample from "./reactExercises/exercise6/UpdateExample";
-import Header from "./reactExercises/exercise7/Header";
-import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7";
 import ReactExercise8 from "./reactExercises/exercise8/ReactExercise8";
+import Exercise2 from "./exercises/exercise2/Exercise2";
+import Exercise3 from "./exercises/exercise3/Exercise3";
+import Exercise4 from "./exercises/exercise4/Exercise4";
+import Exercise5 from "./exercises/exercise5/Exercise5";
+import Exercise13 from "./exercises/exercise13/Exercise13";
+import ReactExercise4 from "./reactExercises/exercise4/ReactExercise4";
+import ReactExercise5 from "./reactExercises/exercise5/ReactExercise5";
+
+import Exercise12 from "./exercises/exercise12/Exercise12";
+import TodoApp from "./experiments/TodoApp";
+import ReactExercise7 from "./reactExercises/exercise7/ReactExercise7";
+import HomePage from "./HomePage";
+
 import ReactExercise9 from "./reactExercises/exercise9/ReactExercise9";
 
+import ReactExercise6 from "./reactExercises/exercise6/ReactExercise6";
+import ReactExercise3 from "./reactExercises/exercise3/ReactExercixe3";
+import TailwindExercise1 from "./tailwindCss/TailwindExercise1";
+
 function App() {
+  const javaScriptExercisesData = [
+    {
+      routesName: "/",
+      component: <HomePage />,
+    },
+    {
+      routesName: "Exercise1",
+      component: <Exercise1 />,
+    },
+    {
+      routesName: "Exercise2",
+      component: <Exercise2 />,
+    },
+    {
+      routesName: "Exercise3",
+      component: <Exercise3 />,
+    },
+    {
+      routesName: "Exercise4",
+      component: <Exercise4 />,
+    },
+    {
+      routesName: "Exercise5",
+      component: <Exercise5 />,
+    },
+    {
+      routesName: "Exercise6",
+      component: <Exercise6 />,
+    },
+    {
+      routesName: "Exercise7",
+      component: <Exercise7 />,
+    },
+    {
+      routesName: "Exercise8",
+      component: <Exercise8 />,
+    },
+    {
+      routesName: "Exercise9",
+      component: <Exercise9 />,
+    },
+    {
+      routesName: "Exercise10",
+      component: <Exercise10 />,
+    },
+    {
+      routesName: "Exercise11",
+      component: <Exercise11 />,
+    },
+    {
+      routesName: "Exercise12",
+      component: <Exercise12 />,
+    },
+    {
+      routesName: "Exercise13",
+      component: <Exercise13 />,
+    },
+    {
+      routesName: "experiment",
+      component: <TodoApp />,
+    },
+  ];
+
+  const reactExercisesData = [
+    {
+      routesName: "ReactExercise1",
+      component: <ReactExercise1 />,
+    },
+    {
+      routesName: "ReactExercise2",
+      component: <ReactExercise2 />,
+    },
+    {
+      routesName: "ReactExercise3",
+      component: <ReactExercise3 />,
+    },
+    {
+      routesName: "ReactExercise4",
+      component: <ReactExercise4 />,
+    },
+    {
+      routesName: "ReactExercise5",
+      component: <ReactExercise5 />,
+    },
+    {
+      routesName: "ReactExercise6",
+      component: <ReactExercise6 />,
+    },
+    {
+      routesName: "ReactExercise7",
+      component: <ReactExercise7 />,
+    },
+    {
+      routesName: "ReactExercise8",
+      component: <ReactExercise8 />,
+    },
+    {
+      routesName: "ReactExercise9",
+      component: <ReactExercise9 />,
+    },
+  ];
+  const tailwindExercisesData = [
+    {
+      routesName: "TailwindExercise1",
+      component: <TailwindExercise1 />,
+    },
+  ];
+
   return (
     <>
-      {/* <Exercise1/> */}
-      {/* <Exercise2/>
-    <Exercise3/>
-    <Exercise4/> 
-    <Exercise5/>
-    <Exercise6/>
-    <Exercise7/>
-    <Exercise8/>
-    <Exercise9/>
-    <Exercise10/>
-    <Exercise11/>
-    <Exercise12/>
-    <Exercise13/> */}
-      {/* <TodoApp /> */}
-      {/* <ReactExercise1 /> */}
-      {/* <Example1 /> */}
-      {/* <Example2 /> */}
-      {/* <ReactExercise2 /> */}
-      {/* <ConditionalRendering /> */}
-      {/* <EventHandling /> */}
-      {/* <PassingArguments /> */}
-      {/* <ReactExercise3 /> */}
-      {/* <ReactExercise4 /> */}
-      {/* <ControlledComponent /> */}
-      {/* <FormHandling /> */}
-      {/* <FormValidation /> */}
-      {/* <ReactExercise5 /> */}
-      {/* <MountExample /> */}
-      {/* <UnmountExample /> */}
-      {/* <UpdateExample/> */}
-      {/* <ReactExercise6 /> */}
-      {/* <ReactExercise7 /> */}
-      {/* <ReactExercise8/> */}
-      {/* <ReactExercise9/> */}
-      
+      <Routes>
+        {javaScriptExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+        {reactExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+        {tailwindExercisesData.map((route) => (
+          <Route
+            key={route.routesName}
+            path={`/${route.routesName}`}
+            element={route.component}
+          />
+        ))}
+      </Routes>
     </>
   );
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,125 @@
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigation = useNavigate();
+  const javaScriptExecisesData = [
+    {
+      routesName: "Exercise1",
+    },
+    {
+      routesName: "Exercise2",
+    },
+    {
+      routesName: "Exercise3",
+    },
+    {
+      routesName: "Exercise4",
+    },
+    {
+      routesName: "Exercise5",
+    },
+    {
+      routesName: "Exercise6",
+    },
+    {
+      routesName: "Exercise7",
+    },
+    {
+      routesName: "Exercise8",
+    },
+    {
+      routesName: "Exercise9",
+    },
+    {
+      routesName: "Exercise10",
+    },
+    {
+      routesName: "Exercise11",
+    },
+    {
+      routesName: "Exercise12",
+    },
+    {
+      routesName: "Exercise13",
+    },
+  ];
+  const reactExecisesData = [
+    {
+      routesName: "ReactExercise1",
+    },
+    {
+      routesName: "ReactExercise2",
+    },
+    {
+      routesName: "ReactExercise3",
+    },
+    {
+      routesName: "ReactExercise4",
+    },
+    {
+      routesName: "ReactExercise5",
+    },
+    {
+      routesName: "ReactExercise6",
+    },
+    {
+      routesName: "ReactExercise7",
+    },
+    {
+      routesName: "ReactExercise8",
+    },
+    {
+      routesName: "ReactExercise9",
+    },
+  ];
+  const tailwindExecisesData = [{ routesName: "TailwindExercise1" }];
+  return (
+    <>
+      <div>
+        <h2> Java Script Exercises</h2>
+        <nav className="mainContainer">
+          {javaScriptExecisesData.map((route) => (
+            <button
+              key={route.routesName}
+              onClick={() => navigation(`/${route.routesName}`)}
+              className="cardButton"
+            >
+              {route.routesName}
+            </button>
+          ))}
+        </nav>
+      </div>
+      <div>
+        <h2> React Exercises</h2>
+        <nav className="mainContainer">
+          {reactExecisesData.map((route) => (
+            <button
+              key={route.routesName}
+              onClick={() => navigation(`/${route.routesName}`)}
+              className="cardButton"
+            >
+              {route.routesName}
+            </button>
+          ))}
+        </nav>
+      </div>
+      {/* This Section code is repesenting Tailwind CSS Exercises */}
+      <div>
+        <h2> Tailwind CSS Exercises</h2>
+        <nav className="mainContainer">
+          {tailwindExecisesData.map((route) => (
+            <button
+              key={route.routesName}
+              onClick={() => navigation(`/${route.routesName}`)}
+              className="cardButton"
+            >
+              {route.routesName}
+            </button>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
