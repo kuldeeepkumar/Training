b2984f42acefa16a936509bf4d9c3ff42c9c3ddb
mehak-d940 <mehakunchibassi3@gmail.com>
2025-07-04 04:44:31 +0000

    packakges glitch is fixed and reinstall is done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import "./index.css";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
